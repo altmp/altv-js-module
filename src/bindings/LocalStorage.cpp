83a42d6f85270d583c10bc068067368d81e5d798
Vektor <whiteman.chr@hotmail.com>
2020-11-18 16:25:41 +0100

    fix storage save

--- src/bindings/LocalStorage.cpp
+++ src/bindings/LocalStorage.cpp
@@ -14,64 +14,46 @@ static void StaticGet(const v8::FunctionCallbackInfo<v8::Value> &info)
 
 static void Get(const v8::FunctionCallbackInfo<v8::Value> &info)
 {
-	V8_GET_ISOLATE_CONTEXT();
+	V8_GET_ISOLATE_CONTEXT_IRESOURCE();
 
 	V8_CHECK_ARGS_LEN(1);
 	V8_ARG_TO_STRING(1, key);
-
-	alt::IResource *resource = V8ResourceImpl::GetResource(ctx);
-	V8_CHECK(resource, "Invalid resource");
 
 	V8_RETURN(V8Helpers::MValueToV8(resource->GetLocalStorage()->Get(key)));
 }
 
 static void Set(const v8::FunctionCallbackInfo<v8::Value> &info)
 {
-	V8_GET_ISOLATE_CONTEXT();
+	V8_GET_ISOLATE_CONTEXT_IRESOURCE();
 
 	V8_CHECK_ARGS_LEN(2);
 
 	V8_ARG_TO_STRING(1, key);
 	V8_ARG_TO_MVALUE(2, val);
 	
-	alt::IResource *resource = V8ResourceImpl::GetResource(ctx);
-	V8_CHECK(resource, "Invalid resource");
-
 	resource->GetLocalStorage()->Set(key, val);
 }
 
 static void Delete(const v8::FunctionCallbackInfo<v8::Value> &info)
 {
-	V8_GET_ISOLATE_CONTEXT();
+	V8_GET_ISOLATE_CONTEXT_IRESOURCE();
 
 	V8_CHECK_ARGS_LEN(1);
 	V8_ARG_TO_STRING(1, key);
-
-	alt::IResource *resource = V8ResourceImpl::GetResource(ctx);
-	V8_CHECK(resource, "Invalid resource");
 
 	resource->GetLocalStorage()->Delete(key);
 }
 
 static void Clear(const v8::FunctionCallbackInfo<v8::Value> &info)
 {
-	V8_GET_ISOLATE_CONTEXT();
-
-	alt::IResource *resource = V8ResourceImpl::GetResource(ctx);
-	V8_CHECK(resource, "Invalid resource");
+	V8_GET_ISOLATE_CONTEXT_IRESOURCE();
 
 	resource->GetLocalStorage()->Clear();
 }
 
 static void Save(const v8::FunctionCallbackInfo<v8::Value> &info)
 {
-	V8_GET_ISOLATE_CONTEXT();
-
-	V8_CHECK_ARGS_LEN(1);
-	V8_ARG_TO_STRING(1, key);
-
-	alt::IResource *resource = V8ResourceImpl::GetResource(ctx);
-	V8_CHECK(resource, "Invalid resource");
+	V8_GET_ISOLATE_CONTEXT_IRESOURCE();
 
 	V8_CHECK(resource->GetLocalStorage()->Save(), "exceeded max local storage size (4MB)");
 }
