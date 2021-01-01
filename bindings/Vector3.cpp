a8c177af79514e95978cc2e8fe078a7fca60c30c
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-01-01 23:04:52 +0100

    Fix Vector3 add method with array

--- bindings/Vector3.cpp
+++ bindings/Vector3.cpp
@@ -92,8 +92,8 @@ static void Add(const v8::FunctionCallbackInfo<v8::Value>& info)
 			V8_CHECK(arr->Length() == 3, "Argument must be an array of 3 numbers");
 			
 			V8_TO_NUMBER(arr->Get(ctx, 0).ToLocalChecked(), x2);
-			V8_TO_NUMBER(arr->Get(ctx, 0).ToLocalChecked(), y2);
-			V8_TO_NUMBER(arr->Get(ctx, 0).ToLocalChecked(), z2);
+			V8_TO_NUMBER(arr->Get(ctx, 1).ToLocalChecked(), y2);
+			V8_TO_NUMBER(arr->Get(ctx, 2).ToLocalChecked(), z2);
 			V8_RETURN(resource->CreateVector3({ x + x2, y + y2, z + z2 }));
 		}
 		else if (arg->IsObject())
