dc33987128efc9696db59da00e769d391bc302ef
Vektor <whiteman.chr@hotmail.com>
2021-01-01 21:54:27 +0100

    fix ws setExtraHeader

--- src/bindings/WebSocketClient.cpp
+++ src/bindings/WebSocketClient.cpp
@@ -114,12 +114,13 @@ static void SetExtraHeader(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
 	V8_GET_ISOLATE_CONTEXT();
 
-	V8_CHECK_ARGS_LEN(1);
-	V8_ARG_TO_STRING(1, msg);
+	V8_CHECK_ARGS_LEN(2);
+	V8_ARG_TO_STRING(1, name);
+	V8_ARG_TO_STRING(2, value);
 
 	V8_GET_THIS_BASE_OBJECT(webSocket, alt::IWebSocketClient);
 
-	webSocket->AddSubProtocol(msg);
+	webSocket->SetExtraHeader(name, value);
 }
 
 static void GetExtraHeaders(const v8::FunctionCallbackInfo<v8::Value>& info)
