e69bee17bbe8eda1d899998c21a675486ba73020
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-07-16 14:50:17 +0200

    Rename object macros

--- V8Helpers.h
+++ V8Helpers.h
@@ -335,13 +335,13 @@ namespace V8
 #define V8_OBJECT_SET_NUMBER(v8Val, prop, val) \
 	(v8Val)->Set(ctx, v8::String::NewFromUtf8(isolate, prop).ToLocalChecked(), v8::Number::New(isolate, val));
 
-#define V8_OBJECT_GET_INTEGER(v8Val, prop, val) \
+#define V8_OBJECT_GET_INT(v8Val, prop, val) \
 	V8_TO_INTEGER((v8Val)->Get(ctx, v8::String::NewFromUtf8(isolate, prop).ToLocalChecked()).ToLocalChecked(), val)
 
-#define V8_OBJECT_SET_INTEGER(v8Val, prop, val) \
+#define V8_OBJECT_SET_INT(v8Val, prop, val) \
 	(v8Val)->Set(ctx, v8::String::NewFromUtf8(isolate, prop).ToLocalChecked(), v8::Integer::New(isolate, val));
 
-#define V8_OBJECT_SET_UNSIGNEDINTEGER(v8Val, prop, val) \
+#define V8_OBJECT_SET_UINT(v8Val, prop, val) \
 	(v8Val)->Set(ctx, v8::String::NewFromUtf8(isolate, prop).ToLocalChecked(), v8::Integer::NewFromUnsigned(isolate, val));
 
 #define V8_OBJECT_GET_BOOLEAN(v8Val, prop, val) \
