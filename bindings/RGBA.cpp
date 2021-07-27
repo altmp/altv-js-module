062a3fcfad37e51e411fe7e4139162b4bc990643
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-07-28 00:28:47 +0200

    Check RGBA values in constructor

--- bindings/RGBA.cpp
+++ bindings/RGBA.cpp
@@ -25,9 +25,13 @@ static void Constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
 	V8_CHECK_ARGS_LEN(4);
 
 	V8_ARG_TO_INT(1, r);
+	V8_CHECK(r > 0 && r < 256, "Invalid RGBA R value. Allowed is 0 - 255");
 	V8_ARG_TO_INT(2, g);
+	V8_CHECK(g > 0 && g < 256, "Invalid RGBA G value. Allowed is 0 - 255");
 	V8_ARG_TO_INT(3, b);
+	V8_CHECK(b > 0 && b < 256, "Invalid RGBA B value. Allowed is 0 - 255");
 	V8_ARG_TO_INT(4, a);
+	V8_CHECK(a > 0 && a < 256, "Invalid RGBA A value. Allowed is 0 - 255");
 
 	V8::DefineOwnProperty(isolate, ctx, info.This(), V8::RGBA_RKey(isolate), v8::Integer::New(isolate, r), v8::PropertyAttribute::ReadOnly);
 	V8::DefineOwnProperty(isolate, ctx, info.This(), V8::RGBA_GKey(isolate), v8::Integer::New(isolate, g), v8::PropertyAttribute::ReadOnly);
