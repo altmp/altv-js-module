2e2d6da13ed6e1ff882be5eda714c843824d9dc6
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-09-18 17:05:41 +0200

    Server: Reformat
    Former-commit-id: c50e37026b6051617cefd30c783008827cdd8a01

--- server/src/bindings/vehicle/Appearance.h
+++ server/src/bindings/vehicle/Appearance.h
@@ -2,154 +2,153 @@
 
 namespace V8::Vehicle
 {
-	void ModKitGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo<v8::Value>& info)
-	{
-		V8_GET_ISOLATE();
-		V8_GET_THIS_BASE_OBJECT(_this, IVehicle);
-		V8_RETURN_INT(_this->GetModKit());
-	}
+    void ModKitGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo<v8::Value>& info)
+    {
+        V8_GET_ISOLATE();
+        V8_GET_THIS_BASE_OBJECT(_this, IVehicle);
+        V8_RETURN_INT(_this->GetModKit());
+    }
 
-	void ModKitSetter(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::PropertyCallbackInfo<void>& info)
-	{
-		V8_GET_ISOLATE_CONTEXT();
-		V8_GET_THIS_BASE_OBJECT(_this, IVehicle);
-		V8_TO_INTEGER(value, _val);
-		V8_CHECK(_this->SetModKit(_val), "wrong modkit");
-	}
+    void ModKitSetter(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::PropertyCallbackInfo<void>& info)
+    {
+        V8_GET_ISOLATE_CONTEXT();
+        V8_GET_THIS_BASE_OBJECT(_this, IVehicle);
+        V8_TO_INTEGER(value, _val);
+        V8_CHECK(_this->SetModKit(_val), "wrong modkit");
+    }
 
-	void NeonActiveGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo<v8::Value>& info)
-	{
-		V8_GET_ISOLATE_CONTEXT();
-		V8_GET_THIS_BASE_OBJECT(_this, IVehicle);
+    void NeonActiveGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo<v8::Value>& info)
+    {
+        V8_GET_ISOLATE_CONTEXT();
+        V8_GET_THIS_BASE_OBJECT(_this, IVehicle);
 
-		bool left, right, front, back;
-		_this->GetNeonActive(&left, &right, &front, &back);
+        bool left, right, front, back;
+        _this->GetNeonActive(&left, &right, &front, &back);
 
-		v8::Local<v8::Object> neonActive = v8::Object::New(isolate);
-		V8::DefineOwnProperty(isolate, ctx, neonActive, "left", v8::Boolean::New(isolate, left));
-		V8::DefineOwnProperty(isolate, ctx, neonActive, "right", v8::Boolean::New(isolate, right));
-		V8::DefineOwnProperty(isolate, ctx, neonActive, "front", v8::Boolean::New(isolate, front));
-		V8::DefineOwnProperty(isolate, ctx, neonActive, "back", v8::Boolean::New(isolate, back));
+        v8::Local<v8::Object> neonActive = v8::Object::New(isolate);
+        V8::DefineOwnProperty(isolate, ctx, neonActive, "left", v8::Boolean::New(isolate, left));
+        V8::DefineOwnProperty(isolate, ctx, neonActive, "right", v8::Boolean::New(isolate, right));
+        V8::DefineOwnProperty(isolate, ctx, neonActive, "front", v8::Boolean::New(isolate, front));
+        V8::DefineOwnProperty(isolate, ctx, neonActive, "back", v8::Boolean::New(isolate, back));
 
-		V8_RETURN(neonActive);
-	}
+        V8_RETURN(neonActive);
+    }
 
-	void NeonActiveSetter(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::PropertyCallbackInfo<void>& info)
-	{
-		V8_GET_ISOLATE_CONTEXT();
-		V8_GET_THIS_BASE_OBJECT(_this, IVehicle);
+    void NeonActiveSetter(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::PropertyCallbackInfo<void>& info)
+    {
+        V8_GET_ISOLATE_CONTEXT();
+        V8_GET_THIS_BASE_OBJECT(_this, IVehicle);
 
-		V8_TO_OBJECT(value, neonActive);
-		V8_OBJECT_GET_BOOLEAN(neonActive, "left", left);
-		V8_OBJECT_GET_BOOLEAN(neonActive, "right", right);
-		V8_OBJECT_GET_BOOLEAN(neonActive, "front", front);
-		V8_OBJECT_GET_BOOLEAN(neonActive, "back", back);
+        V8_TO_OBJECT(value, neonActive);
+        V8_OBJECT_GET_BOOLEAN(neonActive, "left", left);
+        V8_OBJECT_GET_BOOLEAN(neonActive, "right", right);
+        V8_OBJECT_GET_BOOLEAN(neonActive, "front", front);
+        V8_OBJECT_GET_BOOLEAN(neonActive, "back", back);
 
-		_this->SetNeonActive(left, right, front, back);
-	}
+        _this->SetNeonActive(left, right, front, back);
+    }
 
-	void SetWheels(const v8::FunctionCallbackInfo<v8::Value>& info)
-	{
-		V8_GET_ISOLATE_CONTEXT();
-		V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
-		V8_CHECK_ARGS_LEN(2);
+    void SetWheels(const v8::FunctionCallbackInfo<v8::Value>& info)
+    {
+        V8_GET_ISOLATE_CONTEXT();
+        V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
+        V8_CHECK_ARGS_LEN(2);
 
-		V8_ARG_TO_INT(1, type);
-		V8_ARG_TO_INT(2, variation);
+        V8_ARG_TO_INT(1, type);
+        V8_ARG_TO_INT(2, variation);
 
-		vehicle->SetWheels(type, variation);
-	}
+        vehicle->SetWheels(type, variation);
+    }
 
-	void SetRearWheels(const v8::FunctionCallbackInfo<v8::Value>& info)
-	{
-		V8_GET_ISOLATE_CONTEXT();
-		V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
-		V8_CHECK_ARGS_LEN(1);
+    void SetRearWheels(const v8::FunctionCallbackInfo<v8::Value>& info)
+    {
+        V8_GET_ISOLATE_CONTEXT();
+        V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
+        V8_CHECK_ARGS_LEN(1);
 
-		V8_ARG_TO_INT(1, variation);
+        V8_ARG_TO_INT(1, variation);
 
-		vehicle->SetRearWheels(variation);
-	}
+        vehicle->SetRearWheels(variation);
+    }
 
-	void GetModsCount(const v8::FunctionCallbackInfo<v8::Value>& info)
-	{
-		V8_GET_ISOLATE_CONTEXT();
-		V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
-		V8_CHECK_ARGS_LEN(1);
+    void GetModsCount(const v8::FunctionCallbackInfo<v8::Value>& info)
+    {
+        V8_GET_ISOLATE_CONTEXT();
+        V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
+        V8_CHECK_ARGS_LEN(1);
 
-		V8_ARG_TO_INT(1, category);
+        V8_ARG_TO_INT(1, category);
 
-		V8_RETURN_INT(vehicle->GetModsCount(category));
-	}
+        V8_RETURN_INT(vehicle->GetModsCount(category));
+    }
 
-	void SetMod(const v8::FunctionCallbackInfo<v8::Value>& info)
-	{
-		V8_GET_ISOLATE_CONTEXT();
-		V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
-		V8_CHECK_ARGS_LEN(2);
+    void SetMod(const v8::FunctionCallbackInfo<v8::Value>& info)
+    {
+        V8_GET_ISOLATE_CONTEXT();
+        V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
+        V8_CHECK_ARGS_LEN(2);
 
-		V8_ARG_TO_INT(1, category);
-		V8_ARG_TO_INT(2, id);
+        V8_ARG_TO_INT(1, category);
+        V8_ARG_TO_INT(2, id);
 
-		if (!vehicle->SetMod(category, id))
-		{
-			if (vehicle->GetModKit() == 0)
-				V8Helpers::Throw(isolate, "modkit is not set");
-			else
-				V8Helpers::Throw(isolate, "mod doesn't exist");
-		}
-	}
+        if(!vehicle->SetMod(category, id))
+        {
+            if(vehicle->GetModKit() == 0) V8Helpers::Throw(isolate, "modkit is not set");
+            else
+                V8Helpers::Throw(isolate, "mod doesn't exist");
+        }
+    }
 
-	void GetMod(const v8::FunctionCallbackInfo<v8::Value>& info)
-	{
-		V8_GET_ISOLATE_CONTEXT();
-		V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
-		V8_CHECK_ARGS_LEN(1);
+    void GetMod(const v8::FunctionCallbackInfo<v8::Value>& info)
+    {
+        V8_GET_ISOLATE_CONTEXT();
+        V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
+        V8_CHECK_ARGS_LEN(1);
 
-		V8_ARG_TO_INT(1, category);
+        V8_ARG_TO_INT(1, category);
 
-		V8_RETURN_INT(vehicle->GetMod(category));
-	}
+        V8_RETURN_INT(vehicle->GetMod(category));
+    }
 
-	void SetExtra(const v8::FunctionCallbackInfo<v8::Value>& info)
-	{
-		V8_GET_ISOLATE_CONTEXT();
-		V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
-		V8_CHECK_ARGS_LEN(2);
+    void SetExtra(const v8::FunctionCallbackInfo<v8::Value>& info)
+    {
+        V8_GET_ISOLATE_CONTEXT();
+        V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
+        V8_CHECK_ARGS_LEN(2);
 
-		V8_ARG_TO_INT(1, category);
-		V8_ARG_TO_BOOLEAN(2, state);
+        V8_ARG_TO_INT(1, category);
+        V8_ARG_TO_BOOLEAN(2, state);
 
-		vehicle->ToggleExtra(category, state);
-	}
+        vehicle->ToggleExtra(category, state);
+    }
 
-	void GetExtra(const v8::FunctionCallbackInfo<v8::Value>& info)
-	{
-		V8_GET_ISOLATE_CONTEXT();
-		V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
-		V8_CHECK_ARGS_LEN(1);
+    void GetExtra(const v8::FunctionCallbackInfo<v8::Value>& info)
+    {
+        V8_GET_ISOLATE_CONTEXT();
+        V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
+        V8_CHECK_ARGS_LEN(1);
 
-		V8_ARG_TO_INT(1, category);
+        V8_ARG_TO_INT(1, category);
 
-		V8_RETURN_BOOLEAN(vehicle->IsExtraOn(category));
-	}
+        V8_RETURN_BOOLEAN(vehicle->IsExtraOn(category));
+    }
 
-	void SetAppearanceData(const v8::FunctionCallbackInfo<v8::Value>& info)
-	{
-		V8_GET_ISOLATE_CONTEXT();
-		V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
-		V8_CHECK_ARGS_LEN(1);
+    void SetAppearanceData(const v8::FunctionCallbackInfo<v8::Value>& info)
+    {
+        V8_GET_ISOLATE_CONTEXT();
+        V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
+        V8_CHECK_ARGS_LEN(1);
 
-		V8_ARG_TO_STRING(1, data);
+        V8_ARG_TO_STRING(1, data);
 
-		vehicle->LoadAppearanceDataFromBase64(data);
-	}
+        vehicle->LoadAppearanceDataFromBase64(data);
+    }
 
-	void GetAppearanceData(const v8::FunctionCallbackInfo<v8::Value>& info)
-	{
-		V8_GET_ISOLATE();
-		V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
+    void GetAppearanceData(const v8::FunctionCallbackInfo<v8::Value>& info)
+    {
+        V8_GET_ISOLATE();
+        V8_GET_THIS_BASE_OBJECT(vehicle, IVehicle);
 
-		V8_RETURN_STRING(vehicle->GetAppearanceDataBase64().CStr());
-	}
-}
+        V8_RETURN_STRING(vehicle->GetAppearanceDataBase64().CStr());
+    }
+}  // namespace V8::Vehicle
--- server/src/events/Main.cpp
+++ server/src/events/Main.cpp
@@ -16,202 +16,152 @@ using alt::CEvent;
 using EventType = CEvent::Type;
 
 V8::EventHandler clientScriptEvent(
-	EventType::CLIENT_SCRIPT_EVENT,
-	[](V8ResourceImpl* resource, const CEvent* e) {
-		auto ev = static_cast<const alt::CClientScriptEvent*>(e);
-		return resource->GetRemoteHandlers(ev->GetName().ToString());
-	},
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CClientScriptEvent*>(e);
+  EventType::CLIENT_SCRIPT_EVENT,
+  [](V8ResourceImpl* resource, const CEvent* e) {
+      auto ev = static_cast<const alt::CClientScriptEvent*>(e);
+      return resource->GetRemoteHandlers(ev->GetName().ToString());
+  },
+  [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+      auto ev = static_cast<const alt::CClientScriptEvent*>(e);
 
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-		V8Helpers::MValueArgsToV8(ev->GetArgs(), args);
-	}
-);
+      args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+      V8Helpers::MValueArgsToV8(ev->GetArgs(), args);
+  });
 
 V8::EventHandler serverScriptEvent(
-	EventType::SERVER_SCRIPT_EVENT,
-	[](V8ResourceImpl* resource, const CEvent* e) {
-		auto ev = static_cast<const alt::CServerScriptEvent*>(e);
-		return resource->GetLocalHandlers(ev->GetName().ToString());
-	},
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CServerScriptEvent*>(e);
-		V8Helpers::MValueArgsToV8(ev->GetArgs(), args);
-	}
-);
+  EventType::SERVER_SCRIPT_EVENT,
+  [](V8ResourceImpl* resource, const CEvent* e) {
+      auto ev = static_cast<const alt::CServerScriptEvent*>(e);
+      return resource->GetLocalHandlers(ev->GetName().ToString());
+  },
+  [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+      auto ev = static_cast<const alt::CServerScriptEvent*>(e);
+      V8Helpers::MValueArgsToV8(ev->GetArgs(), args);
+  });
 
 V8::EventHandler colshapeEvent(
-	EventType::COLSHAPE_EVENT,
-	[](V8ResourceImpl* resource, const CEvent* e) {
-		auto ev = static_cast<const alt::CColShapeEvent*>(e);
+  EventType::COLSHAPE_EVENT,
+  [](V8ResourceImpl* resource, const CEvent* e) {
+      auto ev = static_cast<const alt::CColShapeEvent*>(e);
 
-		if (ev->GetState())
-			return resource->GetLocalHandlers("entityEnterColshape");
-		else
-			return resource->GetLocalHandlers("entityLeaveColshape");
-	},
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CColShapeEvent*>(e);
+      if(ev->GetState()) return resource->GetLocalHandlers("entityEnterColshape");
+      else
+          return resource->GetLocalHandlers("entityLeaveColshape");
+  },
+  [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+      auto ev = static_cast<const alt::CColShapeEvent*>(e);
 
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetEntity()));
-	}
-);
+      args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+      args.push_back(resource->GetBaseObjectOrNull(ev->GetEntity()));
+  });
 
-V8::LocalEventHandler removeEntity(
-	EventType::REMOVE_ENTITY_EVENT,
-	"removeEntity",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CRemoveEntityEvent*>(e);
+V8::LocalEventHandler removeEntity(EventType::REMOVE_ENTITY_EVENT, "removeEntity", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+    auto ev = static_cast<const alt::CRemoveEntityEvent*>(e);
 
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetEntity()));
-	}
-);
+    args.push_back(resource->GetBaseObjectOrNull(ev->GetEntity()));
+});
 
-V8::LocalEventHandler weaponDamage(
-	EventType::WEAPON_DAMAGE_EVENT,
-	"weaponDamage",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CWeaponDamageEvent*>(e);
-		v8::Isolate* isolate = resource->GetIsolate();
+V8::LocalEventHandler weaponDamage(EventType::WEAPON_DAMAGE_EVENT, "weaponDamage", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+    auto ev = static_cast<const alt::CWeaponDamageEvent*>(e);
+    v8::Isolate* isolate = resource->GetIsolate();
 
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetSource()));
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-		args.push_back(v8::Integer::NewFromUnsigned(isolate, ev->GetWeaponHash()));
-		args.push_back(v8::Integer::NewFromUnsigned(isolate, ev->GetDamageValue()));
-		args.push_back(resource->CreateVector3(ev->GetShotOffset()));
-		args.push_back(v8::Integer::New(isolate, static_cast<int8_t>(ev->GetBodyPart())));
-	}
-);
+    args.push_back(resource->GetBaseObjectOrNull(ev->GetSource()));
+    args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+    args.push_back(v8::Integer::NewFromUnsigned(isolate, ev->GetWeaponHash()));
+    args.push_back(v8::Integer::NewFromUnsigned(isolate, ev->GetDamageValue()));
+    args.push_back(resource->CreateVector3(ev->GetShotOffset()));
+    args.push_back(v8::Integer::New(isolate, static_cast<int8_t>(ev->GetBodyPart())));
+});
 
-V8::LocalEventHandler explosionEvent(
-	EventType::EXPLOSION_EVENT,
-	"explosion",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CExplosionEvent*>(e);
-		v8::Isolate* isolate = resource->GetIsolate();
+V8::LocalEventHandler explosionEvent(EventType::EXPLOSION_EVENT, "explosion", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+    auto ev = static_cast<const alt::CExplosionEvent*>(e);
+    v8::Isolate* isolate = resource->GetIsolate();
 
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetSource()));
-		args.push_back(v8::Integer::New(isolate, static_cast<int8_t>(ev->GetExplosionType())));
-		args.push_back(resource->CreateVector3(ev->GetPosition()));
-		args.push_back(v8::Integer::NewFromUnsigned(isolate, ev->GetExplosionFX()));
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-	}
-);
+    args.push_back(resource->GetBaseObjectOrNull(ev->GetSource()));
+    args.push_back(v8::Integer::New(isolate, static_cast<int8_t>(ev->GetExplosionType())));
+    args.push_back(resource->CreateVector3(ev->GetPosition()));
+    args.push_back(v8::Integer::NewFromUnsigned(isolate, ev->GetExplosionFX()));
+    args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+});
 
-V8::LocalEventHandler fireEvent(
-	EventType::FIRE_EVENT,
-	"startFire",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CFireEvent*>(e);
-		v8::Isolate* isolate = resource->GetIsolate();
+V8::LocalEventHandler fireEvent(EventType::FIRE_EVENT, "startFire", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+    auto ev = static_cast<const alt::CFireEvent*>(e);
+    v8::Isolate* isolate = resource->GetIsolate();
 
-		auto& fires = ev->GetFires();
-		v8::Local<v8::Array> v8fires = v8::Array::New(isolate, fires.GetSize());
+    auto& fires = ev->GetFires();
+    v8::Local<v8::Array> v8fires = v8::Array::New(isolate, fires.GetSize());
 
-		for (int i = 0; i < fires.GetSize(); ++i)
-		{
-			v8::Local<v8::Object> v8fire = v8::Object::New(isolate);
-			v8fire->Set(resource->GetContext(), V8::Fire_PosKey(isolate), resource->CreateVector3(fires[i].position));
-			v8fire->Set(resource->GetContext(), V8::Fire_WeaponKey(isolate), v8::Integer::NewFromUnsigned(isolate, fires[i].weaponHash));
+    for(int i = 0; i < fires.GetSize(); ++i)
+    {
+        v8::Local<v8::Object> v8fire = v8::Object::New(isolate);
+        v8fire->Set(resource->GetContext(), V8::Fire_PosKey(isolate), resource->CreateVector3(fires[i].position));
+        v8fire->Set(resource->GetContext(), V8::Fire_WeaponKey(isolate), v8::Integer::NewFromUnsigned(isolate, fires[i].weaponHash));
 
-			v8fires->Set(resource->GetContext(), i, v8fire);
-		}
+        v8fires->Set(resource->GetContext(), i, v8fire);
+    }
 
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetSource()));
-		args.push_back(v8fires);
-	}
-);
+    args.push_back(resource->GetBaseObjectOrNull(ev->GetSource()));
+    args.push_back(v8fires);
+});
 
-V8::LocalEventHandler startProjectileEvent(
-	EventType::START_PROJECTILE_EVENT,
-	"startProjectile",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CStartProjectileEvent*>(e);
-		v8::Isolate* isolate = resource->GetIsolate();
+V8::LocalEventHandler startProjectileEvent(EventType::START_PROJECTILE_EVENT, "startProjectile", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+    auto ev = static_cast<const alt::CStartProjectileEvent*>(e);
+    v8::Isolate* isolate = resource->GetIsolate();
 
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetSource()));
-		args.push_back(resource->CreateVector3(ev->GetStartPosition()));
-		args.push_back(resource->CreateVector3(ev->GetDirection()));
-		args.push_back(v8::Integer::NewFromUnsigned(isolate, ev->GetAmmoHash()));
-		args.push_back(v8::Integer::NewFromUnsigned(isolate, ev->GetWeaponHash()));
-	}
-);
+    args.push_back(resource->GetBaseObjectOrNull(ev->GetSource()));
+    args.push_back(resource->CreateVector3(ev->GetStartPosition()));
+    args.push_back(resource->CreateVector3(ev->GetDirection()));
+    args.push_back(v8::Integer::NewFromUnsigned(isolate, ev->GetAmmoHash()));
+    args.push_back(v8::Integer::NewFromUnsigned(isolate, ev->GetWeaponHash()));
+});
 
-V8::LocalEventHandler resourceStart(
-	EventType::RESOURCE_START,
-	"anyResourceStart",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CResourceStartEvent*>(e);
-		args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetResource()->GetName().CStr()).ToLocalChecked());
-	}
-);
+V8::LocalEventHandler resourceStart(EventType::RESOURCE_START, "anyResourceStart", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+    auto ev = static_cast<const alt::CResourceStartEvent*>(e);
+    args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetResource()->GetName().CStr()).ToLocalChecked());
+});
 
-V8::LocalEventHandler resourceStop(
-	EventType::RESOURCE_STOP,
-	"anyResourceStop",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CResourceStopEvent*>(e);
-		args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetResource()->GetName().CStr()).ToLocalChecked());
-	}
-);
+V8::LocalEventHandler resourceStop(EventType::RESOURCE_STOP, "anyResourceStop", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+    auto ev = static_cast<const alt::CResourceStopEvent*>(e);
+    args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetResource()->GetName().CStr()).ToLocalChecked());
+});
 
-V8::LocalEventHandler resourceError(
-	EventType::RESOURCE_ERROR,
-	"anyResourceError",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CResourceErrorEvent*>(e);
-		args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetResource()->GetName().CStr()).ToLocalChecked());
-	}
-);
+V8::LocalEventHandler resourceError(EventType::RESOURCE_ERROR, "anyResourceError", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+    auto ev = static_cast<const alt::CResourceErrorEvent*>(e);
+    args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetResource()->GetName().CStr()).ToLocalChecked());
+});
 
-V8::LocalEventHandler syncedMetaChange(
-	EventType::SYNCED_META_CHANGE,
-	"syncedMetaChange",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CSyncedMetaDataChangeEvent*>(e);
+V8::LocalEventHandler syncedMetaChange(EventType::SYNCED_META_CHANGE, "syncedMetaChange", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+    auto ev = static_cast<const alt::CSyncedMetaDataChangeEvent*>(e);
 
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-		args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetKey().CStr()).ToLocalChecked());
-		args.push_back(V8Helpers::MValueToV8(ev->GetVal()));
-		args.push_back(V8Helpers::MValueToV8(ev->GetOldVal()));
-	}
-);
+    args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+    args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetKey().CStr()).ToLocalChecked());
+    args.push_back(V8Helpers::MValueToV8(ev->GetVal()));
+    args.push_back(V8Helpers::MValueToV8(ev->GetOldVal()));
+});
 
-V8::LocalEventHandler streamSyncedMetaChange(
-	EventType::STREAM_SYNCED_META_CHANGE,
-	"streamSyncedMetaChange",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CStreamSyncedMetaDataChangeEvent*>(e);
+V8::LocalEventHandler
+  streamSyncedMetaChange(EventType::STREAM_SYNCED_META_CHANGE, "streamSyncedMetaChange", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+      auto ev = static_cast<const alt::CStreamSyncedMetaDataChangeEvent*>(e);
 
-		args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-		args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetKey().CStr()).ToLocalChecked());
-		args.push_back(V8Helpers::MValueToV8(ev->GetVal()));
-		args.push_back(V8Helpers::MValueToV8(ev->GetOldVal()));
-	}
-);
+      args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+      args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetKey().CStr()).ToLocalChecked());
+      args.push_back(V8Helpers::MValueToV8(ev->GetVal()));
+      args.push_back(V8Helpers::MValueToV8(ev->GetOldVal()));
+  });
 
-V8::LocalEventHandler globalMetaChange(
-	EventType::GLOBAL_META_CHANGE,
-	"globalMetaChange",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CGlobalMetaDataChangeEvent*>(e);
+V8::LocalEventHandler globalMetaChange(EventType::GLOBAL_META_CHANGE, "globalMetaChange", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+    auto ev = static_cast<const alt::CGlobalMetaDataChangeEvent*>(e);
 
-		args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetKey().CStr()).ToLocalChecked());
-		args.push_back(V8Helpers::MValueToV8(ev->GetVal()));
-		args.push_back(V8Helpers::MValueToV8(ev->GetOldVal()));
-	}
-);
+    args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetKey().CStr()).ToLocalChecked());
+    args.push_back(V8Helpers::MValueToV8(ev->GetVal()));
+    args.push_back(V8Helpers::MValueToV8(ev->GetOldVal()));
+});
 
-V8::LocalEventHandler globalSyncedMetaChange(
-	EventType::GLOBAL_SYNCED_META_CHANGE,
-	"globalSyncedMetaChange",
-	[](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-		auto ev = static_cast<const alt::CGlobalSyncedMetaDataChangeEvent*>(e);
+V8::LocalEventHandler
+  globalSyncedMetaChange(EventType::GLOBAL_SYNCED_META_CHANGE, "globalSyncedMetaChange", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+      auto ev = static_cast<const alt::CGlobalSyncedMetaDataChangeEvent*>(e);
 
-		args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetKey().CStr()).ToLocalChecked());
-		args.push_back(V8Helpers::MValueToV8(ev->GetVal()));
-		args.push_back(V8Helpers::MValueToV8(ev->GetOldVal()));
-	}
-);
+      args.push_back(v8::String::NewFromUtf8(resource->GetIsolate(), ev->GetKey().CStr()).ToLocalChecked());
+      args.push_back(V8Helpers::MValueToV8(ev->GetVal()));
+      args.push_back(V8Helpers::MValueToV8(ev->GetOldVal()));
+  });
