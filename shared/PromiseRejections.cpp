79c98738a9676881dff1c8b3d5c64e099e07864b
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-09-18 17:03:46 +0200

    shared: Reformat
    Former-commit-id: 007ae2c07f38305a721da98e5e0cd1706f7f3ea9

--- shared/PromiseRejections.cpp
+++ shared/PromiseRejections.cpp
@@ -4,59 +4,48 @@
 
 #include "PromiseRejections.h"
 
-void V8::PromiseRejections::RejectedWithNoHandler(V8ResourceImpl *resource, v8::PromiseRejectMessage &data)
+void V8::PromiseRejections::RejectedWithNoHandler(V8ResourceImpl* resource, v8::PromiseRejectMessage& data)
 {
-	v8::Isolate *isolate = resource->GetIsolate();
+    v8::Isolate* isolate = resource->GetIsolate();
 
-	queue.push_back(std::make_unique<PromiseRejection>(isolate, data.GetPromise(),
-													   data.GetValue(), V8::SourceLocation::GetCurrent(isolate)));
+    queue.push_back(std::make_unique<PromiseRejection>(isolate, data.GetPromise(), data.GetValue(), V8::SourceLocation::GetCurrent(isolate)));
 }
 
-void V8::PromiseRejections::HandlerAdded(V8ResourceImpl *resource, v8::PromiseRejectMessage &data)
+void V8::PromiseRejections::HandlerAdded(V8ResourceImpl* resource, v8::PromiseRejectMessage& data)
 {
-	v8::Isolate *isolate = resource->GetIsolate();
+    v8::Isolate* isolate = resource->GetIsolate();
 
-	auto newEnd = std::remove_if(
-		queue.begin(), queue.end(),
-		[&](std::unique_ptr<PromiseRejection> &rejection) {
-			return rejection->promise.Get(isolate) == data.GetPromise();
-		});
+    auto newEnd = std::remove_if(queue.begin(), queue.end(), [&](std::unique_ptr<PromiseRejection>& rejection) { return rejection->promise.Get(isolate) == data.GetPromise(); });
 
-	queue.erase(newEnd, queue.end());
+    queue.erase(newEnd, queue.end());
 }
 
-void V8::PromiseRejections::ProcessQueue(V8ResourceImpl *resource)
+void V8::PromiseRejections::ProcessQueue(V8ResourceImpl* resource)
 {
-	v8::Isolate *isolate = resource->GetIsolate();
-	v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
+    v8::Isolate* isolate = resource->GetIsolate();
+    v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
 
-	for (auto &rejection : queue)
-	{
-		auto rejectionMsg = *v8::String::Utf8Value(isolate, rejection->value.Get(isolate)->ToString(ctx).ToLocalChecked());
-		auto fileName = rejection->location.GetFileName();
-		if (rejection->location.GetLineNumber() != 0)
-		{
-			Log::Error << "[V8] Unhandled promise rejection at "
-					   << resource->GetResource()->GetName() << ":" << fileName << ":" << rejection->location.GetLineNumber()
-					   << " (" << rejectionMsg << ")" << Log::Endl;
-		}
-		else
-		{
-			Log::Error << "[V8] Unhandled promise rejection at "
-					   << resource->GetResource()->GetName() << ":" << fileName
-					   << " (" << rejectionMsg << ")" << Log::Endl;
-		}
-		
-		resource->DispatchErrorEvent(
-			rejectionMsg,
-			"",
-			fileName,
-			rejection->location.GetLineNumber());
-	}
+    for(auto& rejection : queue)
+    {
+        auto rejectionMsg = *v8::String::Utf8Value(isolate, rejection->value.Get(isolate)->ToString(ctx).ToLocalChecked());
+        auto fileName = rejection->location.GetFileName();
+        if(rejection->location.GetLineNumber() != 0)
+        {
+            Log::Error << "[V8] Unhandled promise rejection at " << resource->GetResource()->GetName() << ":" << fileName << ":" << rejection->location.GetLineNumber() << " ("
+                       << rejectionMsg << ")" << Log::Endl;
+        }
+        else
+        {
+            Log::Error << "[V8] Unhandled promise rejection at " << resource->GetResource()->GetName() << ":" << fileName << " (" << rejectionMsg << ")" << Log::Endl;
+        }
 
-	queue.clear();
+        resource->DispatchErrorEvent(rejectionMsg, "", fileName, rejection->location.GetLineNumber());
+    }
+
+    queue.clear();
 }
 
-V8::PromiseRejection::PromiseRejection(v8::Isolate *isolate, v8::Local<v8::Promise> _promise, v8::Local<v8::Value> _value, V8::SourceLocation &&_location) : promise(isolate, _promise), value(isolate, _value), location(std::move(_location))
+V8::PromiseRejection::PromiseRejection(v8::Isolate* isolate, v8::Local<v8::Promise> _promise, v8::Local<v8::Value> _value, V8::SourceLocation&& _location)
+    : promise(isolate, _promise), value(isolate, _value), location(std::move(_location))
 {
 }
