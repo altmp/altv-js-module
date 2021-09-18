0607c4e41bd3d379bdaa6a8767375810184d2185
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-09-18 17:05:41 +0200

    Server: Reformat

--- server/src/bindings/Blip.cpp
+++ server/src/bindings/Blip.cpp
@@ -7,34 +7,29 @@ using namespace alt;
 
 static void PointConstructor(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
-	V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
 
-	V8_CHECK_ARGS_LEN(4);
+    V8_CHECK_ARGS_LEN(4);
 
-	V8_ARG_TO_INT(1, type);
-	V8_ARG_TO_NUMBER(2, x);
-	V8_ARG_TO_NUMBER(3, y);
-	V8_ARG_TO_NUMBER(4, z);
+    V8_ARG_TO_INT(1, type);
+    V8_ARG_TO_NUMBER(2, x);
+    V8_ARG_TO_NUMBER(3, y);
+    V8_ARG_TO_NUMBER(4, z);
 
-	alt::Position pos(x, y, z);
+    alt::Position pos(x, y, z);
 
-	Ref<IBlip> blip = ICore::Instance().CreateBlip(nullptr, (alt::IBlip::BlipType)type, pos);
+    Ref<IBlip> blip = ICore::Instance().CreateBlip(nullptr, (alt::IBlip::BlipType)type, pos);
 
-	if (blip)
-		resource->BindEntity(info.This(), blip.Get());
-	else
-		V8Helpers::Throw(isolate, "Failed to create Blip");
+    if(blip) resource->BindEntity(info.This(), blip.Get());
+    else
+        V8Helpers::Throw(isolate, "Failed to create Blip");
 }
 
 extern V8Class v8WorldObject;
-extern V8Class v8Blip("Blip", v8WorldObject, nullptr, [](v8::Local<v8::FunctionTemplate> tpl) {
-	v8::Isolate* isolate = v8::Isolate::GetCurrent();
-});
+extern V8Class v8Blip("Blip", v8WorldObject, nullptr, [](v8::Local<v8::FunctionTemplate> tpl) { v8::Isolate* isolate = v8::Isolate::GetCurrent(); });
 
-extern V8Class v8PointBlip("PointBlip", v8Blip, PointConstructor, [](v8::Local<v8::FunctionTemplate> tpl) {
-	v8::Isolate* isolate = v8::Isolate::GetCurrent();
-});
+extern V8Class v8PointBlip("PointBlip", v8Blip, PointConstructor, [](v8::Local<v8::FunctionTemplate> tpl) { v8::Isolate* isolate = v8::Isolate::GetCurrent(); });
 
 /*static V8Class v8EntityBlip("EntityBlip", "Blip", nullptr, [](v8::Local<v8::FunctionTemplate> tpl) {
-	v8::Isolate* isolate = v8::Isolate::GetCurrent();
+        v8::Isolate* isolate = v8::Isolate::GetCurrent();
 });*/
