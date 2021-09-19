53e02cd46d83126532e00f630cc73887077b826e
xxshady <54737754+xxshady@users.noreply.github.com>
2021-09-19 18:44:52 +0300

    client: Fix 2nd arg name in IsTextureExistInArchetype

--- client/src/bindings/Main.cpp
+++ client/src/bindings/Main.cpp
@@ -158,9 +158,9 @@ static void IsTextureExistInArchetype(const v8::FunctionCallbackInfo<v8::Value>&
 
     V8_CHECK_ARGS_LEN(2);
     V8_ARG_TO_INT(1, modelHash);
-    V8_ARG_TO_STRING(2, modelName);
+    V8_ARG_TO_STRING(2, targetTextureName);
 
-    V8_RETURN_BOOLEAN(nullptr != ICore::Instance().GetTextureFromDrawable(modelHash, modelName));
+    V8_RETURN_BOOLEAN(nullptr != ICore::Instance().GetTextureFromDrawable(modelHash, targetTextureName));
 }
 
 static void RequestIPL(const v8::FunctionCallbackInfo<v8::Value>& info)
