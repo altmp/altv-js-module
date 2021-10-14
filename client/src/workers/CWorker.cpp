833b996dc16ad1bf839d8c946aacff263bae43dc
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-10-14 15:56:03 +0200

    client: Clean up worker code a bit

--- client/src/workers/CWorker.cpp
+++ client/src/workers/CWorker.cpp
@@ -196,16 +196,16 @@ void CWorker::EmitError(const std::string& error)
     EmitToMain("error", args);
 }
 
-void CWorker::HandleMainEventQueue()
+static inline void RunEventQueue(CWorker::EventQueue& queue, CWorker::EventHandlerMap& eventHandlers, std::mutex& queueMutex)
 {
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
     auto context = isolate->GetEnteredOrMicrotaskContext();
-    std::unique_lock<std::mutex> lock(main_queueLock);
+    std::unique_lock<std::mutex> lock(queueMutex);
 
-    while(!main_queuedEvents.empty())
+    while(!queue.empty())
     {
         // Get the event at the front of the queue
-        auto& event = main_queuedEvents.front();
+        auto& event = queue.front();
 
         // Create a vector of the event arguments
         std::vector<v8::Local<v8::Value>> args;
@@ -216,48 +216,25 @@ void CWorker::HandleMainEventQueue()
         }
 
         // Call all handlers with the arguments
-        auto handlers = main_eventHandlers.equal_range(event.first);
+        auto handlers = eventHandlers.equal_range(event.first);
         for(auto it = handlers.first; it != handlers.second; it++)
         {
             it->second.fn.Get(isolate)->Call(context, v8::Undefined(isolate), args.size(), args.data());
-            if(it->second.once) main_eventHandlers.erase(it);
+            if(it->second.once) eventHandlers.erase(it);
         }
 
         // Pop the event from the queue
-        main_queuedEvents.pop();
+        queue.pop();
     }
+}
+void CWorker::HandleMainEventQueue()
+{
+    RunEventQueue(main_queuedEvents, main_eventHandlers, main_queueLock);
 }
 
 void CWorker::HandleWorkerEventQueue()
 {
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
-    auto context = isolate->GetEnteredOrMicrotaskContext();
-    std::unique_lock<std::mutex> lock(worker_queueLock);
-
-    while(!worker_queuedEvents.empty())
-    {
-        // Get the event at the front of the queue
-        auto& event = worker_queuedEvents.front();
-
-        // Create a vector of the event arguments
-        std::vector<v8::Local<v8::Value>> args;
-        args.reserve(event.second.size());
-        for(auto& arg : event.second)
-        {
-            args.push_back(V8Helpers::MValueToV8(arg));
-        }
-
-        // Call all handlers with the arguments
-        auto handlers = worker_eventHandlers.equal_range(event.first);
-        for(auto it = handlers.first; it != handlers.second; it++)
-        {
-            it->second.fn.Get(isolate)->Call(context, v8::Undefined(isolate), args.size(), args.data());
-            if(it->second.once) worker_eventHandlers.erase(it);
-        }
-
-        // Pop the event from the queue
-        worker_queuedEvents.pop();
-    }
+    RunEventQueue(worker_queuedEvents, worker_eventHandlers, worker_queueLock);
 }
 
 CWorker::TimerId CWorker::CreateTimer(v8::Local<v8::Function> callback, uint32_t interval, bool once, V8::SourceLocation&& location)
