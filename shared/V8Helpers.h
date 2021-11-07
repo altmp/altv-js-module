21e366877276c1b4cb96aa506d50f723d6aca6f2
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-11-07 14:15:28 +0100

    client/shared: Move value serialization / deserialization to helpers
    Former-commit-id: 0a8319ee80a71f004ef6b7ff52ce69755686c935

--- shared/V8Helpers.h
+++ shared/V8Helpers.h
@@ -318,6 +318,52 @@ namespace V8
         return *v8::String::Utf8Value(isolate, stackTrace->GetFrame(isolate, 0)->GetScriptName());
     }
 
+    namespace Serialization
+    {
+        // A serialized JavaScript value
+        struct Value
+        {
+            uint8_t* data;
+            size_t size;
+
+            bool Valid() const
+            {
+                return data != nullptr && size > 0;
+            }
+
+            Value() : data(nullptr), size(0) {}
+            Value(uint8_t* data, size_t size) : data(data), size(size) {}
+            ~Value()
+            {
+                // Make sure we free the data here, because V8 transfered ownership of the data to us
+                free(data);
+            }
+        };
+
+        // Serializes a JS value to a binary format
+        // Make sure the context is entered before calling this function
+        inline Value Serialize(v8::Local<v8::Context> context, v8::Local<v8::Value> value)
+        {
+            v8::ValueSerializer serializer(context->GetIsolate());
+            serializer.WriteHeader();
+            if(serializer.WriteValue(context, value).IsNothing()) return Value{};
+            std::pair<uint8_t*, size_t> data = serializer.Release();
+            return Value{ data.first, data.second };
+        }
+
+        // Deserializes a JS value from a binary format
+        // Make sure the context is entered before calling this function
+        inline v8::MaybeLocal<v8::Value> Deserialize(v8::Local<v8::Context> context, const Value& value)
+        {
+            if(!value.Valid()) return v8::MaybeLocal<v8::Value>();
+            v8::ValueDeserializer deserializer(context->GetIsolate(), value.data, value.size);
+            v8::Maybe<bool> header = deserializer.ReadHeader(context);
+            if(header.IsNothing() || header.FromJust() == false) return v8::MaybeLocal<v8::Value>();
+            v8::MaybeLocal<v8::Value> result = deserializer.ReadValue(context);
+            return result;
+        }
+    }  // namespace Serialization
+
 }  // namespace V8
 
 #define V8_GET_ISOLATE() v8::Isolate* isolate = info.GetIsolate()
