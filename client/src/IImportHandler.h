ec757f32df15c9feb72b03db2b3845f1a711d4bf
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-12-19 22:38:12 +0100

    client: Move bytecode logic to IImportHandler
    Former-commit-id: e2d4fc62fcca08cb7f60bf092007c1d2b3a8c584

--- client/src/IImportHandler.h
+++ client/src/IImportHandler.h
@@ -12,6 +12,7 @@ protected:
 
 public:
     bool IsValidModule(const std::string& name);
+    bool IsBytecodeModule(uint8_t* buffer, size_t size);
     std::deque<std::string> GetModuleKeys(const std::string& name);
     std::string GetModulePath(v8::Local<v8::Module> moduleHandle);
     v8::Local<v8::Module> GetModuleFromPath(std::string modulePath);
@@ -20,4 +21,5 @@ public:
     v8::MaybeLocal<v8::Module> ResolveFile(const std::string& name, v8::Local<v8::Module> referrer, alt::IResource* resource);
     v8::MaybeLocal<v8::Module> ResolveModule(const std::string& name, v8::Local<v8::Module> referrer, alt::IResource* resource);
     v8::MaybeLocal<v8::Module> ResolveCode(const std::string& code, const V8Helpers::SourceLocation& location);
+    v8::MaybeLocal<v8::Module> ResolveBytecode(uint8_t* buffer, size_t size);
 };
