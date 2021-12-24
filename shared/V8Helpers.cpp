aeb65522c14d56f19d472ad0c07eb6d34ddd51bc
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-12-25 00:22:19 +0100

    shared: Add CallFunctionWithTimeout helper

--- shared/V8Helpers.cpp
+++ shared/V8Helpers.cpp
@@ -3,6 +3,8 @@
 #include "V8ResourceImpl.h"
 #include "V8Helpers.h"
 #include <climits>
+#include <thread>
+#include <chrono>
 
 bool V8Helpers::TryCatch(const std::function<bool()>& fn)
 {
@@ -351,4 +353,26 @@ alt::String V8Helpers::GetJSValueTypeName(v8::Local<v8::Value> val)
     if(val->IsObject()) return "object";
     else
         return "unknown";
+}
+
+v8::MaybeLocal<v8::Value> V8Helpers::CallFunctionWithTimeout(v8::Local<v8::Function> fn, v8::Local<v8::Context> ctx, std::vector<v8::Local<v8::Value>>& args, uint32_t timeout)
+{
+    v8::Isolate* isolate = ctx->GetIsolate();
+    bool hasTimedOut = false;
+    bool hasFinished = false;
+    std::thread([&]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(timeout));
+        if(hasFinished || isolate->IsExecutionTerminating()) return;
+        hasTimedOut = true;
+        isolate->TerminateExecution();
+    }).detach();
+
+    v8::MaybeLocal<v8::Value> result = fn->Call(ctx, v8::Undefined(isolate), args.size(), args.data());
+    hasFinished = true;
+    if(hasTimedOut)
+    {
+        V8Helpers::Throw(isolate, "Script execution timed out");
+        return v8::MaybeLocal<v8::Value>();
+    }
+    return result;
 }
