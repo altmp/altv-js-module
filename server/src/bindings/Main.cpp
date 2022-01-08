9b6d7f421b82e9a23e8067bd36eb81e956239774
Blue <florian.uaz@web.de>
2022-01-08 12:05:05 +0100

    server: fix build Former-commit-id: e6d434cd053a53246bb61787c84d799edb76eb7d

--- server/src/bindings/Main.cpp
+++ server/src/bindings/Main.cpp
@@ -202,7 +202,7 @@ static void SetSyncedMeta(const v8::FunctionCallbackInfo<v8::Value>& info)
     V8_GET_ISOLATE_CONTEXT();
     V8_CHECK_ARGS_LEN(2);
 
-    V8_ARG_TO_STRING(1, key);
+    V8_ARG_TO_STD_STRING(1, key);
     V8_ARG_TO_MVALUE(2, value);
 
     alt::ICore::Instance().SetSyncedMetaData(key, value);
@@ -213,7 +213,7 @@ static void DeleteSyncedMeta(const v8::FunctionCallbackInfo<v8::Value>& info)
     V8_GET_ISOLATE_CONTEXT();
     V8_CHECK_ARGS_LEN(1);
 
-    V8_ARG_TO_STRING(1, key);
+    V8_ARG_TO_STD_STRING(1, key);
 
     alt::ICore::Instance().DeleteSyncedMetaData(key);
 }
@@ -387,7 +387,8 @@ extern V8Module v8Alt("alt",
                         v8ColshapeCircle,
                         v8ColshapeCuboid,
                         v8ColshapeRectangle },
-                      [](v8::Local<v8::Context> ctx, v8::Local<v8::Object> exports) {
+                      [](v8::Local<v8::Context> ctx, v8::Local<v8::Object> exports)
+                      {
                           v8::Isolate* isolate = ctx->GetIsolate();
 
                           V8Helpers::RegisterFunc(exports, "getResourceMain", &GetResourceMain);
--- server/src/events/Player.cpp
+++ server/src/events/Player.cpp
@@ -18,62 +18,77 @@
 using alt::CEvent;
 using EventType = CEvent::Type;
 
-V8Helpers::LocalEventHandler playerConnect(EventType::PLAYER_CONNECT, "playerConnect", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-    auto ev = static_cast<const alt::CPlayerConnectEvent*>(e);
-    args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-});
+V8Helpers::LocalEventHandler playerConnect(EventType::PLAYER_CONNECT,
+                                           "playerConnect",
+                                           [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args)
+                                           {
+                                               auto ev = static_cast<const alt::CPlayerConnectEvent*>(e);
+                                               args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+                                           });
 
-V8Helpers::LocalEventHandler
-  beforePlayerConnect(EventType::PLAYER_BEFORE_CONNECT, "beforePlayerConnect", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-      auto ev = static_cast<const alt::CPlayerBeforeConnectEvent*>(e);
-      const alt::ConnectionInfo& info = ev->GetConnectionInfo();
-      v8::Isolate* isolate = resource->GetIsolate();
-      v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
-      V8_NEW_OBJECT(infoObj);
-      V8_OBJECT_SET_STD_STRING(infoObj, "name", info.name);
-      V8_OBJECT_SET_STD_STRING(infoObj, "socialID", std::to_string(info.socialId));
-      V8_OBJECT_SET_STD_STRING(infoObj, "hwidHash", std::to_string(info.hwidHash));
-      V8_OBJECT_SET_STD_STRING(infoObj, "hwidExHash", std::to_string(info.hwidExHash));
-      V8_OBJECT_SET_STD_STRING(infoObj, "authToken", info.authToken);
-      V8_OBJECT_SET_BOOLEAN(infoObj, "isDebug", info.isDebug);
-      V8_OBJECT_SET_STD_STRING(infoObj, "branch", info.branch);
-      V8_OBJECT_SET_UINT(infoObj, "build", info.build);
-      V8_OBJECT_SET_STD_STRING(infoObj, "cdnUrl", info.cdnUrl);
-      V8_OBJECT_SET_BIGUINT(infoObj, "passwordHash", info.passwordHash);
-      args.push_back(infoObj);
-      args.push_back(V8Helpers::JSValue(ev->GetReason()));
-  });
+V8Helpers::LocalEventHandler beforePlayerConnect(EventType::PLAYER_BEFORE_CONNECT,
+                                                 "beforePlayerConnect",
+                                                 [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args)
+                                                 {
+                                                     auto ev = static_cast<const alt::CPlayerBeforeConnectEvent*>(e);
+                                                     const alt::ConnectionInfo& info = ev->GetConnectionInfo();
+                                                     v8::Isolate* isolate = resource->GetIsolate();
+                                                     v8::Local<v8::Context> ctx = isolate->GetEnteredOrMicrotaskContext();
+                                                     V8_NEW_OBJECT(infoObj);
+                                                     V8_OBJECT_SET_STD_STRING(infoObj, "name", info.name);
+                                                     V8_OBJECT_SET_STD_STRING(infoObj, "socialID", std::to_string(info.socialId));
+                                                     V8_OBJECT_SET_STD_STRING(infoObj, "hwidHash", std::to_string(info.hwidHash));
+                                                     V8_OBJECT_SET_STD_STRING(infoObj, "hwidExHash", std::to_string(info.hwidExHash));
+                                                     V8_OBJECT_SET_STD_STRING(infoObj, "authToken", info.authToken);
+                                                     V8_OBJECT_SET_BOOLEAN(infoObj, "isDebug", info.isDebug);
+                                                     V8_OBJECT_SET_STD_STRING(infoObj, "branch", info.branch);
+                                                     V8_OBJECT_SET_UINT(infoObj, "build", info.build);
+                                                     V8_OBJECT_SET_STD_STRING(infoObj, "cdnUrl", info.cdnUrl);
+                                                     V8_OBJECT_SET_BIGUINT(infoObj, "passwordHash", info.passwordHash);
+                                                     args.push_back(infoObj);
+                                                     args.push_back(V8Helpers::JSValue(ev->GetReason()));
+                                                 });
 
-V8Helpers::LocalEventHandler playerDisconnect(EventType::PLAYER_DISCONNECT, "playerDisconnect", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-    auto ev = static_cast<const alt::CPlayerDisconnectEvent*>(e);
+V8Helpers::LocalEventHandler playerDisconnect(EventType::PLAYER_DISCONNECT,
+                                              "playerDisconnect",
+                                              [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args)
+                                              {
+                                                  auto ev = static_cast<const alt::CPlayerDisconnectEvent*>(e);
 
-    args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-    args.push_back(V8Helpers::JSValue(ev->GetReason()));
-});
+                                                  args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+                                                  args.push_back(V8Helpers::JSValue(ev->GetReason()));
+                                              });
 
-V8Helpers::LocalEventHandler playerDamage(EventType::PLAYER_DAMAGE, "playerDamage", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-    auto ev = static_cast<const alt::CPlayerDamageEvent*>(e);
-    v8::Isolate* isolate = resource->GetIsolate();
+V8Helpers::LocalEventHandler playerDamage(EventType::PLAYER_DAMAGE,
+                                          "playerDamage",
+                                          [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args)
+                                          {
+                                              auto ev = static_cast<const alt::CPlayerDamageEvent*>(e);
+                                              v8::Isolate* isolate = resource->GetIsolate();
 
-    args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-    args.push_back(resource->GetBaseObjectOrNull(ev->GetAttacker()));
-    args.push_back(V8Helpers::JSValue(ev->GetHealthDamage()));
-    args.push_back(V8Helpers::JSValue(ev->GetArmourDamage()));
-    args.push_back(V8Helpers::JSValue(ev->GetWeapon()));
-});
+                                              args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+                                              args.push_back(resource->GetBaseObjectOrNull(ev->GetAttacker()));
+                                              args.push_back(V8Helpers::JSValue(ev->GetHealthDamage()));
+                                              args.push_back(V8Helpers::JSValue(ev->GetArmourDamage()));
+                                              args.push_back(V8Helpers::JSValue(ev->GetWeapon()));
+                                          });
 
-V8Helpers::LocalEventHandler playerDeath(EventType::PLAYER_DEATH, "playerDeath", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-    auto ev = static_cast<const alt::CPlayerDeathEvent*>(e);
-    v8::Isolate* isolate = resource->GetIsolate();
+V8Helpers::LocalEventHandler playerDeath(EventType::PLAYER_DEATH,
+                                         "playerDeath",
+                                         [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args)
+                                         {
+                                             auto ev = static_cast<const alt::CPlayerDeathEvent*>(e);
+                                             v8::Isolate* isolate = resource->GetIsolate();
 
-    args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-    args.push_back(resource->GetBaseObjectOrNull(ev->GetKiller()));
-    args.push_back(V8Helpers::JSValue(ev->GetWeapon()));
-});
+                                             args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+                                             args.push_back(resource->GetBaseObjectOrNull(ev->GetKiller()));
+                                             args.push_back(V8Helpers::JSValue(ev->GetWeapon()));
+                                         });
 
 V8Helpers::LocalEventHandler playerEnterVehicle(EventType::PLAYER_ENTER_VEHICLE,
                                                 "playerEnteredVehicle",  // TODO: change name for consistency
-                                                [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+                                                [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args)
+                                                {
                                                     auto ev = static_cast<const alt::CPlayerEnterVehicleEvent*>(e);
 
                                                     args.push_back(resource->GetBaseObjectOrNull(ev->GetPlayer()));
@@ -83,7 +98,8 @@ V8Helpers::LocalEventHandler playerEnterVehicle(EventType::PLAYER_ENTER_VEHICLE,
 
 V8Helpers::LocalEventHandler playerEnteringVehicle(EventType::PLAYER_ENTERING_VEHICLE,
                                                    "playerEnteringVehicle",  // TODO: don't change names, it's okay
-                                                   [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+                                                   [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args)
+                                                   {
                                                        auto ev = static_cast<const alt::CPlayerEnteringVehicleEvent*>(e);
 
                                                        args.push_back(resource->GetBaseObjectOrNull(ev->GetPlayer()));
@@ -93,7 +109,8 @@ V8Helpers::LocalEventHandler playerEnteringVehicle(EventType::PLAYER_ENTERING_VE
 
 V8Helpers::LocalEventHandler playerLeaveVehicle(EventType::PLAYER_LEAVE_VEHICLE,
                                                 "playerLeftVehicle",  // TODO: change name for consistency
-                                                [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+                                                [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args)
+                                                {
                                                     auto ev = static_cast<const alt::CPlayerLeaveVehicleEvent*>(e);
 
                                                     args.push_back(resource->GetBaseObjectOrNull(ev->GetPlayer()));
@@ -103,7 +120,8 @@ V8Helpers::LocalEventHandler playerLeaveVehicle(EventType::PLAYER_LEAVE_VEHICLE,
 
 V8Helpers::LocalEventHandler playerChangeVehicleSeat(EventType::PLAYER_CHANGE_VEHICLE_SEAT,
                                                      "playerChangedVehicleSeat",  // TODO: change name for consistency
-                                                     [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
+                                                     [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args)
+                                                     {
                                                          auto ev = static_cast<const alt::CPlayerChangeVehicleSeatEvent*>(e);
 
                                                          args.push_back(resource->GetBaseObjectOrNull(ev->GetPlayer()));
@@ -112,21 +130,26 @@ V8Helpers::LocalEventHandler playerChangeVehicleSeat(EventType::PLAYER_CHANGE_VE
                                                          args.push_back(V8Helpers::JSValue(ev->GetNewSeat()));
                                                      });
 
-V8Helpers::LocalEventHandler
-  playerWeaponChange(EventType::PLAYER_WEAPON_CHANGE, "playerWeaponChange", [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-      auto ev = static_cast<const alt::CPlayerWeaponChangeEvent*>(e);
+V8Helpers::LocalEventHandler playerWeaponChange(EventType::PLAYER_WEAPON_CHANGE,
+                                                "playerWeaponChange",
+                                                [](V8ResourceImpl* resource, const CEvent* e, std::vector<v8::Local<v8::Value>>& args)
+                                                {
+                                                    auto ev = static_cast<const alt::CPlayerWeaponChangeEvent*>(e);
 
-      args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-      args.push_back(V8Helpers::JSValue(ev->GetOldWeapon()));
-      args.push_back(V8Helpers::JSValue(ev->GetNewWeapon()));
-  });
+                                                    args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+                                                    args.push_back(V8Helpers::JSValue(ev->GetOldWeapon()));
+                                                    args.push_back(V8Helpers::JSValue(ev->GetNewWeapon()));
+                                                });
 
-V8_LOCAL_EVENT_HANDLER localMetaChange(EventType::LOCAL_SYNCED_META_CHANGE, "localMetaChange", [](V8ResourceImpl* resource, const alt::CEvent* e, std::vector<v8::Local<v8::Value>>& args) {
-    auto ev = static_cast<const alt::CLocalMetaDataChangeEvent*>(e);
-    v8::Isolate* isolate = resource->GetIsolate();
+V8_LOCAL_EVENT_HANDLER localMetaChange(EventType::LOCAL_SYNCED_META_CHANGE,
+                                       "localMetaChange",
+                                       [](V8ResourceImpl* resource, const alt::CEvent* e, std::vector<v8::Local<v8::Value>>& args)
+                                       {
+                                           auto ev = static_cast<const alt::CLocalMetaDataChangeEvent*>(e);
+                                           v8::Isolate* isolate = resource->GetIsolate();
 
-    args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
-    args.push_back(V8Helpers::JSValue(ev->GetKey().CStr()));
-    args.push_back(V8Helpers::MValueToV8(ev->GetVal()));
-    args.push_back(V8Helpers::MValueToV8(ev->GetOldVal()));
-});
+                                           args.push_back(resource->GetBaseObjectOrNull(ev->GetTarget()));
+                                           args.push_back(V8Helpers::JSValue(ev->GetKey()));
+                                           args.push_back(V8Helpers::MValueToV8(ev->GetVal()));
+                                           args.push_back(V8Helpers::MValueToV8(ev->GetOldVal()));
+                                       });
