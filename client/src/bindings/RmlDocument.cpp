68bc54543108752a7502820c5386cc2e8bd79558
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-01-11 23:14:33 +0100

    client: Add missing rml document methods
    Former-commit-id: d638798701f62819827414a4c8a6d5d5c765637e

--- client/src/bindings/RmlDocument.cpp
+++ client/src/bindings/RmlDocument.cpp
@@ -34,14 +34,6 @@ static void TitleSetter(v8::Local<v8::String>, v8::Local<v8::Value> val, const v
     document->SetTitle(title.ToString());
 }
 
-static void SourceUrlGetter(v8::Local<v8::String>, const v8::PropertyCallbackInfo<v8::Value>& info)
-{
-    V8_GET_ISOLATE_CONTEXT();
-    V8_GET_THIS_BASE_OBJECT(document, alt::IRmlDocument);
-
-    V8_RETURN_STRING(document->GetSourceUrl().c_str());
-}
-
 static void VisibleSetter(v8::Local<v8::String>, v8::Local<v8::Value> val, const v8::PropertyCallbackInfo<void>& info)
 {
     V8_GET_ISOLATE_CONTEXT();
@@ -62,20 +54,52 @@ static void Show(const v8::FunctionCallbackInfo<v8::Value>& info)
     document->Show(isModal, focused);
 }
 
+static void CreateElement(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_GET_THIS_BASE_OBJECT(document, alt::IRmlDocument);
+    V8_CHECK_ARGS_LEN(1);
+
+    V8_ARG_TO_STD_STRING(1, tag);
+
+    alt::Ref<alt::IRmlElement> element = document->CreateElement(tag);
+    V8_CHECK(!element.IsEmpty(), "Failed to create element");
+
+    V8_RETURN(resource->GetBaseObjectOrNull(element));
+}
+
+static void CreateTextNode(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_GET_THIS_BASE_OBJECT(document, alt::IRmlDocument);
+    V8_CHECK_ARGS_LEN(1);
+
+    V8_ARG_TO_STD_STRING(1, text);
+
+    alt::Ref<alt::IRmlElement> node = document->CreateTextNode(text);
+    V8_CHECK(!node.IsEmpty(), "Failed to create text node");
+
+    V8_RETURN(resource->GetBaseObjectOrNull(node));
+}
+
 extern V8Class v8RmlElement;
 extern V8Class v8RmlDocument("RmlDocument", v8RmlElement, &DocumentConstructor, [](v8::Local<v8::FunctionTemplate> tpl) {
     using namespace alt;
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
     V8Helpers::SetAccessor<alt::IRmlDocument, const std::string&, &alt::IRmlDocument::GetTitle, &alt::IRmlDocument::SetTitle>(isolate, tpl, "title");
-    V8Helpers::SetAccessor(isolate, tpl, "sourceUrl", &SourceUrlGetter);
+    V8Helpers::SetAccessor<alt::IRmlDocument, const std::string&, &alt::IRmlDocument::GetSourceUrl>(isolate, tpl, "sourceUrl");
     V8Helpers::SetAccessor<alt::IRmlDocument, bool, &alt::IRmlDocument::IsVisible>(isolate, tpl, "isVisible");
     V8Helpers::SetAccessor<alt::IRmlDocument, bool, &alt::IRmlDocument::IsModal>(isolate, tpl, "isModal");
+
+    V8Helpers::SetAccessor<alt::IRmlDocument, Ref<IRmlElement>, &alt::IRmlDocument::GetBody>(isolate, tpl, "body");
+    V8Helpers::SetAccessor<alt::IRmlDocument, Ref<IRmlElement>, &alt::IRmlDocument::GetHead>(isolate, tpl, "head");
 
     V8Helpers::SetMethod(isolate, tpl, "show", &Show);
     V8Helpers::SetMethod<alt::IRmlDocument, &alt::IRmlDocument::Hide>(isolate, tpl, "hide");
 
     V8Helpers::SetMethod<alt::IRmlDocument, &alt::IRmlDocument::Update>(isolate, tpl, "update");
 
-    // todo: add CreateElement & CreateTextNode
+    V8Helpers::SetMethod(isolate, tpl, "createElement", &CreateElement);
+    V8Helpers::SetMethod(isolate, tpl, "createTextNode", &CreateTextNode);
 });
