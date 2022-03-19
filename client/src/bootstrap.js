e8bbd4394708bfc20cfcece0d5c7be5a2ec46604
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-03-19 21:36:13 +0100

    client: Use alt functions instead of global variable
    Former-commit-id: 7b8145daf64fb44304f8ced1866bd5a848a4829d

--- client/src/bootstrap.js
+++ client/src/bootstrap.js
@@ -4,6 +4,6 @@ import * as alt from "alt-client";
 // Load the global bindings code
 new Function("alt", __internal_bindings_code)(alt);
 
-let mainPath = __internal_main_path;
+let mainPath = alt.getResourceMain(alt.resourceName);
 if(mainPath[0] !== "/") mainPath = "/" + mainPath;
 await import(mainPath);
