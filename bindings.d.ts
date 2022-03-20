ad08e6df047d11ec1ab89b7da75f42e0fb19ddea
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-03-20 16:46:56 +0100

    shared/client: Add main bindings and Utils class

--- bindings.d.ts
+++ bindings.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="@altv/types-shared"/>
+/// <reference types="@altv/types-natives"/>
 import * as altShared from "alt-shared";
+import * as altNatives from "natives";
 
 declare global {
     export const alt: typeof altShared;
+    export const native: typeof altNatives;
 }
--- client/src/bootstrap.js
+++ client/src/bootstrap.js
@@ -1,8 +1,9 @@
 // clang-format off
 import * as alt from "alt-client";
+import * as native from "natives";
 
 // Load the global bindings code
-new Function("alt", __internal_bindings_code)(alt);
+new Function("alt", "native", __internal_bindings_code)(alt, native);
 
 let mainPath = alt.Resource.current.main;
 if(mainPath[0] !== "/") mainPath = "/" + mainPath;
