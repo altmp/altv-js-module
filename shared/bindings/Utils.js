b5aa17af8b5137c8d9c21da0b374399e3012ca98
YannBCF <Yannbeats.pro@gmail.com>
2022-03-24 18:51:37 +0100

    shared: Fixes

--- shared/bindings/Utils.js
+++ shared/bindings/Utils.js
@@ -5,17 +5,20 @@
 // Client only
 if(alt.isClient) {
     alt.Utils.requestModel = async function(model, timeout = 1000) {
+        const _model = model;
         if (typeof model !== "string" && typeof model !== "number") throw new Error("Expected a string or number as first argument");
         if (typeof timeout !== "number") throw new Error("Expected a number as second argument");
 
         if (typeof model === "string") model = alt.hash(model);
-        if (!native.isModelValid(model)) throw new Error("Model not valid");
+        if (!native.isModelValid(model)) throw new Error(typeof _model === "string"
+            ? `Model '${_model}', with hash ${alt.hash(_model)} is invalid`
+            : `Model ${_model} is invalid`);
 
         const checkUntil = Date.now() + timeout;
         native.requestModel(model);
 
         while (!native.hasModelLoaded(model)) {
-            if (Date.now() > checkUntil) throw new Error("Failed to request model");
+            if (Date.now() > checkUntil) throw new Error(`Failed to request model '${_model}'`);
             await alt.Utils.wait(5);
         }
     }
@@ -24,13 +27,13 @@ if(alt.isClient) {
         if (typeof animDict !== "string") throw new Error("Expected a string as first argument");
         if (typeof timeout !== "number") throw new Error("Expected a number as second argument");
 
-        if (!native.doesAnimDictExist(animDict)) throw new Error("Anim dict not valid");
+        if (!native.doesAnimDictExist(animDict)) throw new Error(`Anim dict '${animDict}' not valid`);
 
         const checkUntil = Date.now() + timeout;
         native.requestAnimDict(animDict);
 
         while (!native.hasAnimDictLoaded(animDict)) {
-            if (Date.now() > checkUntil) throw new Error("Failed to request anim dict");
+            if (Date.now() > checkUntil) throw new Error(`Failed to request anim dict '${animDict}'`);
             await alt.Utils.wait(5);
         }
     }
@@ -43,7 +46,7 @@ if(alt.isClient) {
         native.requestAnimSet(animSet);
 
         while (!native.hasAnimSetLoaded(animSet)) {
-            if (Date.now() > checkUntil) throw new Error("Failed to request anim set");
+            if (Date.now() > checkUntil) throw new Error(`Failed to request anim set '${animSet}'`);
             await alt.Utils.wait(5);
         }
     }
@@ -56,21 +59,22 @@ if(alt.isClient) {
         native.requestClipSet(clipSet);
 
         while (!native.hasClipSetLoaded(clipSet)) {
-            if (Date.now() > checkUntil) throw new Error("Failed to request clip set");
+            if (Date.now() > checkUntil) throw new Error(`Failed to request clip set '${clipSet}'`);
             await alt.Utils.wait(5);
         }
     }
 
     alt.Utils.requestCutscene = async function(cutsceneName, flags, timeout = 1000) {
         if (typeof cutsceneName !== "string") throw new Error("Expected a string as first argument");
-        if (typeof flags !== "number") throw new Error("Expected a number as second argument");
+        if (typeof flags !== "number" && typeof flags !== "string") 
+            throw new Error("Expected a number or string as second argument");
         if (typeof timeout !== "number") throw new Error("Expected a number as third argument");
 
         const checkUntil = Date.now() + timeout;
-        native.requestCutscene(cutsceneName, flags);
+        native.requestCutscene(cutsceneName, typeof flags === "string" ? parseInt(flags) : flags);
 
         while (!native.hasCutsceneLoaded(cutsceneName)) {
-            if (Date.now() > checkUntil) throw new Error("Failed to request cutscene");
+            if (Date.now() > checkUntil) throw new Error(`Failed to request cutscene '${cutsceneName}'`);
             await alt.Utils.wait(5);
         }
     }
