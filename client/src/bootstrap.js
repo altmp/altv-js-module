72d2a94e40dbca573080236dba9dd349a854d28a
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-04-06 00:56:02 +0200

    client: Fix exceptions in top level code stack trace

--- client/src/bootstrap.js
+++ client/src/bootstrap.js
@@ -7,4 +7,10 @@ new Function("alt", "native", __internal_bindings_code)(alt, native);
 
 let mainPath = alt.Resource.current.main;
 if(mainPath[0] !== "/") mainPath = "/" + mainPath;
-await import(mainPath);
+try {
+    await import(mainPath);
+}
+catch(e) {
+    let location = `${alt.Resource.current.name}:${mainPath}:${e.lineNumber ?? 0}`;
+    alt.logError(`[V8] Exception at ${location} (${e.message ?? "Unknown error"}) ${e.stack ?? ""}`);
+}
