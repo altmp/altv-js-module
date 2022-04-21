7534de40457528fc5826680b000078ff5c79e63d
Vektor <whiteman.chr@hotmail.com>
2022-04-21 10:23:34 +0200

    client: add discord api functions

--- client/src/bindings/Discord.cpp
+++ client/src/bindings/Discord.cpp
@@ -25,8 +25,38 @@ static void CurrentUserGetter(v8::Local<v8::String>, const v8::PropertyCallbackI
     }
 }
 
+static void RequestOAuth2Token(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT();
+
+    V8_CHECK_ARGS_LEN(1);
+    V8_CHECK(info[0]->IsString(), "zoomDataId must be a number or string");
+
+    V8_ARG_TO_STRING(1, appid);
+
+    V8_RETURN(alt::ICore::Instance().DiscordRequestOAuth2Token(appid));
+}
+
+static void IsOAuth2Finished(v8::Local<v8::String>, const v8::PropertyCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE();
+
+    V8_RETURN(alt::ICore::Instance().DiscordIsOAuth2Finished());
+}
+
+static void GetOAuth2Token(v8::Local<v8::String>, const v8::PropertyCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE();
+
+    V8_RETURN_STRING(alt::ICore::Instance().DiscordGetOAuth2Token());
+}
+
 extern V8Class v8Discord("Discord", [](v8::Local<v8::FunctionTemplate> tpl) {
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
     V8Helpers::SetStaticAccessor(isolate, tpl, "currentUser", &CurrentUserGetter);
+
+    V8Helpers::SetStaticMethod(isolate, tpl, "requestOAuth2Token", &RequestOAuth2Token);
+    V8Helpers::SetStaticAccessor(isolate, tpl, "hasOAuth2Finished", &IsOAuth2Finished);
+    V8Helpers::SetStaticAccessor(isolate, tpl, "getOAuth2Token", &GetOAuth2Token);
 });
