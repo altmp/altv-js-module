78591beb6d7713331d3132bde81cbc55ceeb1fce
Leon B <leonmrbonnie@hotmail.com>
2022-08-06 14:59:40 +0200

    client: Add object api (#182)
    * client: Add weapon API
    
    * client: Add more object api
    
    * client: Add object detach and attach

--- client/src/bindings/Main.cpp
+++ client/src/bindings/Main.cpp
@@ -1051,11 +1051,11 @@ static void IsPointOnScreen(const v8::FunctionCallbackInfo<v8::Value>& info)
 
 extern V8Module sharedModule;
 extern V8Class v8Player, v8Player, v8Vehicle, v8WebView, v8HandlingData, v8LocalStorage, v8MemoryBuffer, v8MapZoomData, v8Discord, v8Voice, v8WebSocketClient, v8Checkpoint, v8HttpClient,
-  v8Audio, v8LocalPlayer, v8Profiler, v8Worker, v8RmlDocument, v8RmlElement, v8WeaponData, v8FocusData;
+  v8Audio, v8LocalPlayer, v8Profiler, v8Worker, v8RmlDocument, v8RmlElement, v8WeaponData, v8FocusData, v8Object;
 extern V8Module altModule("alt",
                           &sharedModule,
                           { v8Player,     v8Vehicle,    v8WebView, v8HandlingData, v8LocalStorage, v8MemoryBuffer, v8MapZoomData, v8Discord,    v8Voice,      v8WebSocketClient,
-                            v8Checkpoint, v8HttpClient, v8Audio,   v8LocalPlayer,  v8Profiler,     v8Worker,       v8RmlDocument, v8RmlElement, v8WeaponData, v8FocusData },
+                            v8Checkpoint, v8HttpClient, v8Audio,   v8LocalPlayer,  v8Profiler,     v8Worker,       v8RmlDocument, v8RmlElement, v8WeaponData, v8FocusData, v8Object },
                           [](v8::Local<v8::Context> ctx, v8::Local<v8::Object> exports)
                           {
                               v8::Isolate* isolate = ctx->GetIsolate();
