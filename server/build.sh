ae9bcc8517a20c0b318d1358a05799e6c7390ba2
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-10-03 21:23:02 +0200

    general: Fix CI CDN upload

--- server/build.sh
+++ server/build.sh
@@ -3,3 +3,7 @@ cd build-linux
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-9 -DJS_MODULE_VERSION=DEV ..
 cmake --build . --config Release
 cd ..
+
+mkdir dist
+cp ./build-linux/libjs-module.so dist
+cp ./deps/nodejs/lib/libnode.so.102 dist
