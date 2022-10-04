a3623fa0f1ffa71cfac265a8395005dbb4dcf296
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-10-04 23:03:31 +0200

    shared: Make JS bindings generation even more automated

--- tools/convert-bindings.js
+++ tools/convert-bindings.js
@@ -1,10 +1,12 @@
 // clang-format off
 const fs = require("fs").promises;
 const pathUtil = require("path");
-const util = require("util");
 
 // Base path should point to the main directory of the repo
-if (process.argv.length < 3) process.exit(1);
+if (process.argv.length < 3) {
+    showLog("Missing 'basePath' argument, exiting...");
+    process.exit(1);
+}
 const basePath = process.argv[2];
 
 // Paths to search for JS bindings
@@ -14,34 +16,93 @@ const paths = [
     "server/src/bindings/"
 ];
 
-// Template for binding output text
-const template = `#pragma once
-namespace {Name}Bindings {
+// Per binding template output text
+const bindingTemplate = `
+namespace {Name} {
     static constexpr char source[] = { {Chars},'\\0' };
 }
 `;
 
+// Full output path
+const resultTemplate = `// !!! THIS FILE WAS AUTOMATICALLY GENERATED, DO NOT EDIT MANUALLY !!!
+#pragma once
+#include <string>
+#include <array>
+
+namespace Bindings {
+    {BindingNamespaces}
+}
+
+namespace JSBindings {
+    static std::string GetBindingsCode()
+    {
+        using namespace Bindings;
+        static constexpr std::array<const char*, {BindingsSize}> bindings = { {BindingNames} };
+        std::string result;
+        result.reserve({BindingsCodeSize});
+        for(const char* binding : bindings) result += binding;
+        return result;
+    }
+}
+`;
+
+// Result bindings output path
+const outputPath = "shared/JSBindings.h";
+
 (async() => {
+    // todo: support client/server only bindings
     showLog("Generating bindings...");
+    const bindings = [];
     for (const path of paths) {
         const bindingsPath = pathUtil.resolve(__dirname, basePath, path);
         for await(const file of getBindingFiles(bindingsPath)) {
             const fullFilePath = pathUtil.resolve(bindingsPath, file);
-            const fileContent = await fs.readFile(fullFilePath, "utf8");
-            // Convert the whole file content to a char code array
-            const chars = fileContent.split("").map((char) => char.charCodeAt(0));
-            const bindingName = pathUtil.basename(fullFilePath, ".js");
-            // Fill output template with our data
-            const outputStr = template
-                .replace("{Name}", bindingName)
-                .replace("{Chars}", chars.toString());
-            const fileName = `${bindingName}.out.h`;
-            // Write output file
-            await fs.writeFile(pathUtil.resolve(bindingsPath, fileName), outputStr);
-            showLog(`Wrote bindings to file: ${fileName}`);
+            // Generate the binding data
+            const binding = await generateBinding(fullFilePath);
+            bindings.push(binding);
+            showLog(`Generated bindings for: ${file}`);
         }
     }
+    const outputStr = generateBindingsResult(bindings);
+    await fs.writeFile(pathUtil.resolve(__dirname, basePath, outputPath), outputStr);
+    showLog(`Wrote bindings result to file: ${outputPath}`);
 })();
+
+function generateBindingsResult(bindings) {
+    let bindingsNamespaces = "";
+    let bindingsNames = "";
+    let bindingsCodeSize = 0;
+    for(let i = 0; i < bindings.length; i++) {
+        const { name, text } = bindings[i];
+        const isLastBinding = i == bindings.length - 1;
+
+        bindingsNamespaces += text;
+        // For array that contains all bindings source texts
+        bindingsNames += `${name}::source`;
+        if(!isLastBinding) bindingsNames += ",";
+        bindingsCodeSize += text.length;
+    }
+    return resultTemplate
+        .replace("{BindingNamespaces}", bindingsNamespaces)
+        .replace("{BindingNames}", bindingsNames)
+        .replace("{BindingsCodeSize}", bindingsCodeSize)
+        .replace("{BindingsSize}", bindings.length);
+}
+
+async function generateBinding(path) {
+    const fileContent = await fs.readFile(path, "utf8");
+    // Convert the whole file content to a char code array
+    const chars = fileContent.split("").map((char) => char.charCodeAt(0));
+    const bindingName = pathUtil.basename(path, ".js");
+    // Fill output template with our data
+    const outputStr = bindingTemplate
+        .replace("{Name}", bindingName)
+        .replace("{Chars}", chars.toString());
+    return {
+        name: bindingName,
+        text: outputStr
+    };
+}
 
 // Recursively gets all binding files in the directory, returns an async iterator
 async function* getBindingFiles(dir) {
