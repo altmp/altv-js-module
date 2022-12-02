f9d051ed44988a8c450abe7658e524043bfaf694
Blue <florian.uaz@web.de>
2022-12-02 13:21:33 +0100

    shared|server: add possible nullptr checks

--- shared/bindings/Entity.cpp
+++ shared/bindings/Entity.cpp
@@ -257,7 +257,12 @@ static void StaticAllGetter(v8::Local<v8::String>, const v8::PropertyCallbackInf
     uint16_t i = 0;
     for(auto entity : alt::ICore::Instance().GetEntities())
     {
-        if(entity) arr->Set(ctx, i++, resource->GetEntity(entity, "Entity")->GetJSVal(isolate));
+        if(entity)
+        {
+            V8Entity* ent = resource->GetEntity(entity, "Entity");
+            if (ent == nullptr) continue;
+            arr->Set(ctx, i++, ent->GetJSVal(isolate));
+        }
     };
 
     V8_RETURN(arr);
