b385b7e077d7a0652a5cc33edfb69fec4211b414
xshady <54737754+xxshady@users.noreply.github.com>
2022-12-27 16:16:17 +0300

    shared: Add alt.off function reference validation (#234)
    * shared: Add alt.off function reference validation
    
    * Revert formatOnSave

--- shared/V8ResourceImpl.h
+++ shared/V8ResourceImpl.h
@@ -11,6 +11,7 @@
 #include "V8Timer.h"
 
 #include "IRuntimeEventHandler.h"
+#include "V8Helpers.h"
 
 class V8ResourceImpl : public alt::IResource::Impl
 {
@@ -69,14 +70,28 @@ public:
         remoteGenericHandlers.push_back(V8Helpers::EventCallback{ isolate, cb, std::move(location), once });
     }
 
-    void UnsubscribeLocal(const std::string& ev, v8::Local<v8::Function> cb)
+    void UnsubscribeLocal(const std::string& ev, v8::Local<v8::Function> cb, V8Helpers::SourceLocation&& location)
     {
         auto range = localHandlers.equal_range(ev);
+        bool anyHandlerRemoved = false;
 
         for(auto it = range.first; it != range.second; ++it)
         {
-            if(it->second.fn.Get(isolate)->StrictEquals(cb)) it->second.removed = true;
+            if(it->second.fn.Get(isolate)->StrictEquals(cb))
+            {
+                it->second.removed = true;
+                anyHandlerRemoved = true;
+            }
         }
+
+        if(!anyHandlerRemoved)
+        {
+            Log::Warning << 
+                location.ToString() << " alt.off was called for event \"" << ev << 
+                "\" with function reference that was not subscribed" << Log::Endl;
+            return;
+        }
+
         alt::CEvent::Type type = V8Helpers::EventHandler::GetTypeForEventName(ev);
         if(type != alt::CEvent::Type::NONE) IRuntimeEventHandler::Instance().EventHandlerRemoved(type);
     }
