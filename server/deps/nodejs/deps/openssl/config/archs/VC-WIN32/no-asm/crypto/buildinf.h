11241ca236dac901fdfbdb12c874f9d4870d0100
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-01-25 23:08:48 +0100

    server: Update to NodeJS v18

--- server/deps/nodejs/deps/openssl/config/archs/VC-WIN32/no-asm/crypto/buildinf.h
+++ server/deps/nodejs/deps/openssl/config/archs/VC-WIN32/no-asm/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: "
-#define DATE "built on: Tue Dec 14 23:09:35 2021 UTC"
+#define DATE "built on: Thu Nov  3 02:21:06 2022 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
--- server/deps/nodejs/deps/openssl/config/archs/linux-ppc64le/asm_avx2/crypto/buildinf.h
+++ server/deps/nodejs/deps/openssl/config/archs/linux-ppc64le/asm_avx2/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux-ppc64le"
-#define DATE "built on: Tue Dec 14 23:02:08 2021 UTC"
+#define DATE "built on: Thu Nov  3 02:18:00 2022 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
