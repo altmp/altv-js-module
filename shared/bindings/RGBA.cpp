21988b5a406659e679dd64d4269baeb46e21505d
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-02-11 02:36:12 +0100

    shared: Add JS value to map helper and allow recursive conversion

--- shared/bindings/RGBA.cpp
+++ shared/bindings/RGBA.cpp
@@ -27,25 +27,12 @@ static void Constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
         else if(info[0]->IsObject())
         {
             v8::Local<v8::Object> obj = info[0].As<v8::Object>();
-            v8::Local<v8::Value> val;
-            V8_CHECK(obj->Get(ctx, V8Helpers::RGBA_RKey(isolate)).ToLocal(&val), "Invalid object value at key 'r'");
-            V8_CHECK(val->IsNumber(), "Invalid object value type at key 'r'");
-            r = val->Int32Value(ctx).FromMaybe(0);
-            V8_CHECK(obj->Get(ctx, V8Helpers::RGBA_GKey(isolate)).ToLocal(&val), "Invalid object value at key 'g'");
-            V8_CHECK(val->IsNumber(), "Invalid object value type at key 'g'");
-            g = val->Int32Value(ctx).FromMaybe(0);
-            V8_CHECK(obj->Get(ctx, V8Helpers::RGBA_BKey(isolate)).ToLocal(&val), "Invalid object value at key 'b'");
-            V8_CHECK(val->IsNumber(), "Invalid object value type at key 'b'");
-            b = val->Int32Value(ctx).FromMaybe(0);
-
-            if(obj->Has(ctx, V8Helpers::RGBA_AKey(isolate)).FromMaybe(false))
-            {
-                V8_CHECK(obj->Get(ctx, V8Helpers::RGBA_AKey(isolate)).ToLocal(&val), "Invalid object value at key 'a'");
-                V8_CHECK(val->IsNumber(), "Invalid object value type at key 'a'");
-                a = val->Int32Value(ctx).FromMaybe(255);
-            }
-            else
-                a = 255;
+            std::optional<std::unordered_map<std::string, int>> values = V8Helpers::CppValue<int>(obj);
+            V8_CHECK(values.has_value(), "Invalid object passed");
+            r = values->at("r");
+            g = values->at("g");
+            b = values->at("b");
+            if(values->contains("a")) a = values->at("a");
         }
         else
         {
