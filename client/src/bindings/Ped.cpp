55175dcae13b5ec57cd8455a76c5f743ce1a40b9
Blue <florian.uaz@web.de>
2023-04-08 14:49:49 +0200

    server|client: Add Ped getByID getter

--- client/src/bindings/Ped.cpp
+++ client/src/bindings/Ped.cpp
@@ -11,6 +11,25 @@ static void AllGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo
     V8_RETURN(resource->GetAllPeds());
 }
 
+static void StaticGetByID(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_CHECK_ARGS_LEN(1);
+
+    V8_ARG_TO_INT(1, id);
+
+    alt::IEntity* entity = alt::ICore::Instance().GetEntityByID(id);
+
+    if(entity && entity->GetType() == alt::IEntity::Type::PED)
+    {
+        V8_RETURN_BASE_OBJECT(entity);
+    }
+    else
+    {
+        V8_RETURN_NULL();
+    }
+}
+
 // clang-format off
 extern V8Class v8Entity;
 extern V8Class v8Ped("Ped", v8Entity, [](v8::Local<v8::FunctionTemplate> tpl)
@@ -18,6 +37,7 @@ extern V8Class v8Ped("Ped", v8Entity, [](v8::Local<v8::FunctionTemplate> tpl)
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
     V8Helpers::SetStaticAccessor(isolate, tpl, "all", &AllGetter);
+    V8Helpers::SetStaticMethod(isolate, tpl, "getByID", StaticGetByID);
 
     V8Helpers::SetAccessor<IPed, uint32_t, &IPed::GetCurrentWeapon>(isolate, tpl, "currentWeapon");
     V8Helpers::SetAccessor<IPed, uint16_t, &IPed::GetHealth>(isolate, tpl, "health");
--- server/src/bindings/Ped.cpp
+++ server/src/bindings/Ped.cpp
@@ -39,6 +39,25 @@ static void AllGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo
     V8_RETURN(resource->GetAllPeds());
 }
 
+static void StaticGetByID(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_CHECK_ARGS_LEN(1);
+
+    V8_ARG_TO_INT(1, id);
+
+    alt::IEntity* entity = alt::ICore::Instance().GetEntityByID(id);
+
+    if(entity && entity->GetType() == alt::IEntity::Type::PED)
+    {
+        V8_RETURN_BASE_OBJECT(entity);
+    }
+    else
+    {
+        V8_RETURN_NULL();
+    }
+}
+
 // clang-format off
 extern V8Class v8Entity;
 extern V8Class v8Ped("Ped", v8Entity, Constructor, [](v8::Local<v8::FunctionTemplate> tpl)
@@ -46,6 +65,8 @@ extern V8Class v8Ped("Ped", v8Entity, Constructor, [](v8::Local<v8::FunctionTemp
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
     V8Helpers::SetStaticAccessor(isolate, tpl, "all", &AllGetter);
+    V8Helpers::SetStaticMethod(isolate, tpl, "getByID", &StaticGetByID);
+
     V8Helpers::SetAccessor<IPed, uint32_t, &IPed::GetCurrentWeapon, &IPed::SetCurrentWeapon>(isolate, tpl, "currentWeapon");
     V8Helpers::SetAccessor<IPed, uint16_t, &IPed::GetHealth, &IPed::SetHealth>(isolate, tpl, "health");
     V8Helpers::SetAccessor<IPed, uint16_t, &IPed::GetMaxHealth, &IPed::SetMaxHealth>(isolate, tpl, "maxHealth");
