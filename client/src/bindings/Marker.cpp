50288b31f0c8e033d467098607882693fffcdd0b
Blue <florian.uaz@web.de>
2023-04-08 23:11:24 +0200

    client: rename marker rotation getter/setter

--- client/src/bindings/Marker.cpp
+++ client/src/bindings/Marker.cpp
@@ -79,6 +79,6 @@ extern V8Class v8Marker("Marker",
                             V8Helpers::SetAccessor(isolate, tpl, "markerType", &TypeGetter, &TypeSetter);
                             V8Helpers::SetAccessor<IMarker, RGBA, &IMarker::GetColor, &IMarker::SetColor>(isolate, tpl, "color");
                             V8Helpers::SetAccessor<IMarker, Position, &IMarker::GetScale, &IMarker::SetScale>(isolate, tpl, "scale");
-                            V8Helpers::SetAccessor<IMarker, Rotation, &IMarker::GetRotation, &IMarker::SetRotation>(isolate, tpl, "rotation");
+                            V8Helpers::SetAccessor<IMarker, Rotation, &IMarker::GetRotation, &IMarker::SetRotation>(isolate, tpl, "rot");
                             V8Helpers::SetAccessor<IMarker, Position, &IMarker::GetDirection, &IMarker::SetDirection>(isolate, tpl, "direction");
                         });
