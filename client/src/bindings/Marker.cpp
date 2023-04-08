3ae0e0fa32317bc7789e6143ffca0c1bb2bbafa9
Blue <florian.uaz@web.de>
2023-04-08 21:42:20 +0200

    client: add Marker getByID

--- client/src/bindings/Marker.cpp
+++ client/src/bindings/Marker.cpp
@@ -43,6 +43,25 @@ static void TypeSetter(v8::Local<v8::String>, v8::Local<v8::Value> val, const v8
     marker->SetMarkerType((alt::IMarker::MarkerType)markertype);
 }
 
+static void StaticGetByID(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_CHECK_ARGS_LEN(1);
+
+    V8_ARG_TO_INT(1, id);
+
+    alt::IBaseObject* baseObject = alt::ICore::Instance().GetBaseObjectByID(alt::IBaseObject::Type::MARKER, id);
+
+    if(baseObject && baseObject->GetType() == alt::IEntity::Type::MARKER)
+    {
+        V8_RETURN_BASE_OBJECT(baseObject);
+    }
+    else
+    {
+        V8_RETURN_NULL();
+    }
+}
+
 extern V8Class v8WorldObject;
 extern V8Class v8Marker("Marker",
                         v8WorldObject,
@@ -53,6 +72,7 @@ extern V8Class v8Marker("Marker",
                             v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
                             V8Helpers::SetStaticAccessor(isolate, tpl, "all", &AllGetter);
+                            V8Helpers::SetStaticMethod(isolate, tpl, "getByID", StaticGetByID);
 
                             V8Helpers::SetAccessor<IMarker, uint32_t, &IMarker::GetID>(isolate, tpl, "id");
                             V8Helpers::SetAccessor<IMarker, bool, &IMarker::GetVisible, &IMarker::SetVisible>(isolate, tpl, "visible");
