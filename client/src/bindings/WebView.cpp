79b0060e58d93ca78597cbfbd3cb6742adad4d52
Blue <florian.uaz@web.de>
2023-04-16 13:57:59 +0200

    client: add webview getByID

--- client/src/bindings/WebView.cpp
+++ client/src/bindings/WebView.cpp
@@ -226,6 +226,25 @@ static void AllWebviewGetter(v8::Local<v8::String> name, const v8::PropertyCallb
     V8_RETURN(jsArr);
 }
 
+static void StaticGetByID(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_CHECK_ARGS_LEN(1);
+
+    V8_ARG_TO_INT(1, id);
+
+    alt::IBaseObject* baseObject = alt::ICore::Instance().GetBaseObjectByID(alt::IBaseObject::Type::WEBVIEW, id);
+
+    if(baseObject && baseObject->GetType() == alt::IEntity::Type::WEBVIEW)
+    {
+        V8_RETURN_BASE_OBJECT(baseObject);
+    }
+    else
+    {
+        V8_RETURN_NULL();
+    }
+}
+
 extern V8Class v8BaseObject;
 extern V8Class v8WebView("WebView",
                          v8BaseObject,
@@ -235,6 +254,7 @@ extern V8Class v8WebView("WebView",
                              v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
                              V8Helpers::SetStaticAccessor(isolate, tpl, "all", &AllWebviewGetter);
+                             V8Helpers::SetStaticMethod(isolate, tpl, "getByID", StaticGetByID);
                              V8Helpers::SetMethod(isolate, tpl, "toString", ToString);
 
                              V8Helpers::SetAccessor<IWebView, uint32_t, &IWebView::GetID>(isolate, tpl, "id");
