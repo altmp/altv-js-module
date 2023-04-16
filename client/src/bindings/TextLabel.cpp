8106f1fc3b2bb06c5ccad7cc41a7dbe1c703bec5
Blue <florian.uaz@web.de>
2023-04-16 13:56:08 +0200

    client: add textlabel getByID

--- client/src/bindings/TextLabel.cpp
+++ client/src/bindings/TextLabel.cpp
@@ -29,6 +29,25 @@ static void Constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
 //     V8_RETURN(resource->GetAllTextLabel());
 // }
 
+static void StaticGetByID(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_CHECK_ARGS_LEN(1);
+
+    V8_ARG_TO_INT(1, id);
+
+    alt::IBaseObject* baseObject = alt::ICore::Instance().GetBaseObjectByID(alt::IBaseObject::Type::TEXT_LABEL, id);
+
+    if(baseObject && baseObject->GetType() == alt::IEntity::Type::TEXT_LABEL)
+    {
+        V8_RETURN_BASE_OBJECT(baseObject);
+    }
+    else
+    {
+        V8_RETURN_NULL();
+    }
+}
+
 extern V8Class v8WorldObject;
 extern V8Class v8TextLabel("TextLabel",
                            v8WorldObject,
@@ -39,6 +58,7 @@ extern V8Class v8TextLabel("TextLabel",
                                v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
                                // V8Helpers::SetStaticAccessor(isolate, tpl, "all", &AllGetter);
+                               V8Helpers::SetStaticMethod(isolate, tpl, "getByID", StaticGetByID);
 
                                V8Helpers::SetAccessor<ITextLabel, uint32_t, &ITextLabel::GetID>(isolate, tpl, "id");
                                V8Helpers::SetAccessor<ITextLabel, bool, &ITextLabel::IsRemote>(isolate, tpl, "isRemote");
