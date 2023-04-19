6008c1df229bff294eefeabe0b2254997ab0bcd1
Blue <florian.uaz@web.de>
2023-04-19 16:24:57 +0200

    client: enable dimension setter

--- shared/bindings/WorldObject.cpp
+++ shared/bindings/WorldObject.cpp
@@ -8,12 +8,12 @@
 using namespace alt;
 
 extern V8Class v8BaseObject;
-extern V8Class v8WorldObject("WorldObject", v8BaseObject, [](v8::Local<v8::FunctionTemplate> tpl) {
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
+extern V8Class v8WorldObject("WorldObject",
+                             v8BaseObject,
+                             [](v8::Local<v8::FunctionTemplate> tpl)
+                             {
+                                 v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
-    V8Helpers::SetAccessor<IWorldObject, Position, &IWorldObject::GetPosition, &IWorldObject::SetPosition>(isolate, tpl, "pos");
-
-#ifdef ALT_SERVER_API
-    V8Helpers::SetAccessor<IWorldObject, int32_t, &IWorldObject::GetDimension, &IWorldObject::SetDimension>(isolate, tpl, "dimension");
-#endif  // ALT_SERVER_API
-});
+                                 V8Helpers::SetAccessor<IWorldObject, Position, &IWorldObject::GetPosition, &IWorldObject::SetPosition>(isolate, tpl, "pos");
+                                 V8Helpers::SetAccessor<IWorldObject, int32_t, &IWorldObject::GetDimension, &IWorldObject::SetDimension>(isolate, tpl, "dimension");
+                             });
