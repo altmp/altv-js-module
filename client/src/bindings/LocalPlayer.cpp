3d6c50687ffc2c54fa4b104cc0993856cd384a06
Blue <florian.uaz@web.de>
2023-04-26 19:26:46 +0200

    client: add Localplayer pos setter

--- client/src/bindings/LocalPlayer.cpp
+++ client/src/bindings/LocalPlayer.cpp
@@ -129,4 +129,5 @@ extern V8Class v8LocalPlayer("LocalPlayer",
                                  V8Helpers::SetAccessor<alt::ILocalPlayer, float, &alt::ILocalPlayer::GetStamina, &alt::ILocalPlayer::SetStamina>(isolate, tpl, "stamina");
                                  V8Helpers::SetAccessor<alt::ILocalPlayer, float, &alt::ILocalPlayer::GetMaxStamina, &alt::ILocalPlayer::SetMaxStamina>(isolate, tpl, "maxStamina");
                                  V8Helpers::SetAccessor<alt::IWorldObject, int32_t, &alt::IWorldObject::GetDimension>(isolate, tpl, "dimension");
+                                 V8Helpers::SetAccessor<alt::IWorldObject, alt::Position, &alt::IWorldObject::GetPosition, &alt::IWorldObject::SetPosition>(isolate, tpl, "pos");
                              });
