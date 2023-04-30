aa58ef5d85d5066aae21c01754f0aee4fa573a71
Vektor <whiteman.chr@hotmail.com>
2023-04-30 19:31:41 +0200

    fix object ctor

--- client/src/bindings/Object.cpp
+++ client/src/bindings/Object.cpp
@@ -18,7 +18,7 @@ static void Constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     V8_GET_ISOLATE_CONTEXT_RESOURCE();
     V8_CHECK_CONSTRUCTOR();
-    V8_CHECK_ARGS_LEN_MIN_MAX(3, 5);
+    V8_CHECK_ARGS_LEN_MIN_MAX(3, 7);
 
     uint32_t modelHash;
     if(info[0]->IsString())
