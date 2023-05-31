0b7c84d09d4370b753f0ca9926097d323e87ae02
Vadim Zubkov <me@vadzz.dev>
2023-05-31 16:19:06 +0300

    client: Removed unused stuff

--- client/src/bindings/Font.cpp
+++ client/src/bindings/Font.cpp
@@ -8,45 +8,6 @@
 
 using namespace alt;
 
-//static void ToString(const v8::FunctionCallbackInfo<v8::Value>& info)
-//{
-//    V8_GET_ISOLATE_CONTEXT();
-//
-//    auto font = info.This();
-//
-//    std::ostringstream ss;
-//    ss << "Font{ }";
-//
-//    V8_RETURN_STRING(ss.str());
-//}
-
-static void Constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
-{
-    auto& core = alt::ICore::Instance();
-
-    V8_GET_ISOLATE_CONTEXT_RESOURCE();
-    V8_CHECK_CONSTRUCTOR();
-    V8_CHECK_ARGS_LEN(3);
-
-    V8_ARG_TO_STRING(1, txd);
-    V8_ARG_TO_STRING(2, name);
-
-    std::shared_ptr<v8::BackingStore> buffer;
-    if(info[0]->IsArrayBufferView())
-    {
-        V8_ARG_TO_ARRAY_BUFFER_VIEW(3, arrayBufferView);
-        buffer = arrayBufferView->Buffer()->GetBackingStore();
-    }
-    else
-    {
-        V8_ARG_TO_ARRAY_BUFFER(3, arrayBuffer);
-        buffer = arrayBuffer->GetBackingStore();
-    }
-
-    ICustomTexture* texture = core.CreateCustomTextureFromImage(txd, name, (uint8_t*)buffer->Data(), buffer->ByteLength(), resource->GetResource());
-    V8_BIND_BASE_OBJECT(texture, "Failed to create CustomTexture");
-}
-
 static void StaticRegister(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     V8_GET_ISOLATE_CONTEXT_RESOURCE();
