b672985c488a2fc80c4a6a1377bcd8e240ff3708
Artem Dzhemesiuk <programming@zziger.me>
2023-06-06 00:22:19 +0200

    client: implement alt.Ped.getByScriptID (#276)

--- client/src/bindings/Ped.cpp
+++ client/src/bindings/Ped.cpp
@@ -35,6 +35,24 @@ static void StaticGetByID(const v8::FunctionCallbackInfo<v8::Value>& info)
     }
 }
 
+static void StaticGetByScriptID(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_CHECK_ARGS_LEN(1);
+    V8_ARG_TO_INT(1, scriptGuid);
+
+    alt::IWorldObject* entity = alt::ICore::Instance().GetWorldObjectByScriptID(scriptGuid);
+
+    if(entity && (entity->GetType() == alt::IWorldObject::Type::PED || entity->GetType() == alt::IWorldObject::Type::LOCAL_PED))
+    {
+        V8_RETURN_BASE_OBJECT(entity);
+    }
+    else
+    {
+        V8_RETURN_NULL();
+    }
+}
+
 // clang-format off
 extern V8Class v8Entity;
 extern V8Class v8Ped("Ped", v8Entity, [](v8::Local<v8::FunctionTemplate> tpl)
@@ -44,6 +62,7 @@ extern V8Class v8Ped("Ped", v8Entity, [](v8::Local<v8::FunctionTemplate> tpl)
     V8Helpers::SetStaticAccessor(isolate, tpl, "all", &AllGetter);
     V8Helpers::SetStaticAccessor(isolate, tpl, "count", &CountGetter);
     V8Helpers::SetStaticMethod(isolate, tpl, "getByID", StaticGetByID);
+    V8Helpers::SetStaticMethod(isolate, tpl, "getByScriptID", StaticGetByScriptID);
 
     V8Helpers::SetAccessor<IPed, uint32_t, &IPed::GetCurrentWeapon>(isolate, tpl, "currentWeapon");
     V8Helpers::SetAccessor<IPed, uint16_t, &IPed::GetHealth>(isolate, tpl, "health");
