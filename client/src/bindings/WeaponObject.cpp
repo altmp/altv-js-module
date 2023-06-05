08789f574b1178b24d16ef83c68338d1bd28a8cc
Vektor <whiteman.chr@hotmail.com>
2023-06-05 22:23:52 +0200

    fixes

--- client/src/bindings/WeaponObject.cpp
+++ client/src/bindings/WeaponObject.cpp
@@ -12,37 +12,46 @@ static void Constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
     uint32_t weaponHash;
     if(info[0]->IsString())
     {
-        V8_ARG_TO_STRING(3, weaponhash);
+        V8_ARG_TO_STRING(1, weaponhash);
         weaponHash = alt::ICore::Instance().Hash(weaponhash);
     }
     else
     {
-        V8_ARG_TO_UINT(3, _weaponhash);
+        V8_ARG_TO_UINT(1, _weaponhash);
         weaponHash = _weaponhash;
     }
+    
+    V8_ARG_TO_VECTOR3(2, pos);
+    V8_ARG_TO_VECTOR3(3, rot);
 
-    uint32_t modelHash;
-    if(info[0]->IsString())
+    if (info.Length() < 4)
     {
-        V8_ARG_TO_STRING(4, modelhash);
-        modelHash = alt::ICore::Instance().Hash(modelhash);
+        auto obj = alt::ICore::Instance().CreateWeaponObject(pos, rot, weaponHash, 0, 100, true, 1.f, false, 0, resource->GetResource());
+        V8_BIND_BASE_OBJECT(obj, "Failed to create weaponObject");
     }
     else
     {
-        V8_ARG_TO_UINT_OPT(4, _modelHash, 0);
-        modelHash = _modelHash;
+        uint32_t modelHash;
+        if (info[3]->IsString())
+        {
+            V8_ARG_TO_STRING(4, modelhash);
+            modelHash = alt::ICore::Instance().Hash(modelhash);
+        }
+        else
+        {
+            V8_ARG_TO_UINT(4, _modelHash);
+            modelHash = _modelHash;
+        }
+
+        V8_ARG_TO_INT_OPT(5, numAmmo, 100);
+        V8_ARG_TO_BOOLEAN_OPT(6, createDefaultComponents, true);
+        V8_ARG_TO_NUMBER_OPT(7, scale, 1);
+        V8_ARG_TO_BOOLEAN_OPT(8, useStreaming, false);
+        V8_ARG_TO_UINT_OPT(9, streamingDistance, 0);
+
+        auto obj = alt::ICore::Instance().CreateWeaponObject(pos, rot, weaponHash, modelHash, numAmmo, createDefaultComponents, scale, useStreaming, streamingDistance, resource->GetResource());
+        V8_BIND_BASE_OBJECT(obj, "Failed to create weaponObject");
     }
-
-    V8_ARG_TO_VECTOR3(1, pos);
-    V8_ARG_TO_VECTOR3(2, rot);
-    V8_ARG_TO_INT_OPT(5, numAmmo, 100);
-    V8_ARG_TO_BOOLEAN_OPT(6, createDefaultComponents, true);
-    V8_ARG_TO_NUMBER_OPT(7, scale, 1);
-    V8_ARG_TO_BOOLEAN_OPT(8, useStreaming, false);
-    V8_ARG_TO_UINT_OPT(9, streamingDistance, 0);
-
-    auto obj = alt::ICore::Instance().CreateWeaponObject(pos, rot, weaponHash, modelHash, numAmmo, createDefaultComponents, scale, useStreaming, streamingDistance, resource->GetResource());
-    V8_BIND_BASE_OBJECT(obj, "Failed to create weaponObject");
 }
 
 static void SetComponentTintIndex(const v8::FunctionCallbackInfo<v8::Value>& info)
@@ -90,6 +99,17 @@ static void RemoveComponent(const v8::FunctionCallbackInfo<v8::Value>& info)
     object->RemoveComponent(componentType);
 }
 
+static void AllGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_RETURN(resource->GetAllObjects());
+}
+
+static void CountGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo<v8::Value>& info)
+{
+    V8_RETURN_UINT(alt::ICore::Instance().GetWeaponObjects().size());
+}
+
 extern V8Class v8Object;
 extern V8Class v8WeaponObject("WeaponObject",
                               v8Object,
@@ -99,8 +119,9 @@ extern V8Class v8WeaponObject("WeaponObject",
                                   using namespace alt;
                                   v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
-                                  V8Helpers::SetAccessor<IObject, bool, &IObject::IsWeaponObject>(isolate, tpl, "isWeaponObject");
-
+                                  V8Helpers::SetStaticAccessor(isolate, tpl, "all", &AllGetter);
+                                  V8Helpers::SetStaticAccessor(isolate, tpl, "count", &CountGetter);
+                                  
                                   V8Helpers::SetAccessor<IObject, int, &IObject::GetTintIndex, &IObject::SetTintIndex>(isolate, tpl, "tintIndex");
                                   V8Helpers::SetMethod(isolate, tpl, "setComponentTintIndex", &GetComponentTintIndex);
                                   V8Helpers::SetMethod(isolate, tpl, "getComponentTintIndex", &SetComponentTintIndex);
