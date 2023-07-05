ffc0c51a9d0022f0cb3d178f58537cefdc3ec5a9
Vektor <whiteman.chr@hotmail.com>
2023-07-05 16:18:44 +0200

    fix attached output

--- client/src/bindings/AudioOutput.cpp
+++ client/src/bindings/AudioOutput.cpp
@@ -3,7 +3,7 @@
 #include "V8ResourceImpl.h"
 #include "V8Class.h"
 #include "../CV8ScriptRuntime.h"
-#include "cpp-sdk/script-objects/IAudio.h"
+#include "cpp-sdk/objects/IWorldObject.h"
 #include "cpp-sdk/script-objects/IAudioOutput.h"
 #include "cpp-sdk/script-objects/IAudioFrontendOutput.h"
 #include "cpp-sdk/script-objects/IAudioWorldOutput.h"
@@ -20,9 +20,11 @@ static void ConstructorFrontend(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     V8_GET_ISOLATE_CONTEXT_RESOURCE();
     V8_CHECK_CONSTRUCTOR();
-    V8_CHECK_ARGS_LEN(0);
+    V8_CHECK_ARGS_LEN_MIN_MAX(0, 1);
 
-    auto output = alt::ICore::Instance().CreateFrontendOutput(0, resource->GetResource());
+    V8_ARG_TO_INT_OPT(1, categoryHash, alt::ICore::Instance().Hash("radio"));
+
+    auto output = alt::ICore::Instance().CreateFrontendOutput(categoryHash, resource->GetResource());
     V8_BIND_BASE_OBJECT(output, "Failed to create AudioOutputFrontend");
 }
 
@@ -30,11 +32,12 @@ static void ConstructorWorld(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     V8_GET_ISOLATE_CONTEXT_RESOURCE();
     V8_CHECK_CONSTRUCTOR();
-    V8_CHECK_ARGS_LEN(1);
+    V8_CHECK_ARGS_LEN_MIN_MAX(1, 2);
 
     V8_ARG_TO_VECTOR3(1, pos);
+    V8_ARG_TO_INT_OPT(2, categoryHash, alt::ICore::Instance().Hash("radio"));
     
-    auto output = alt::ICore::Instance().CreateWorldOutput(0, pos, resource->GetResource());
+    auto output = alt::ICore::Instance().CreateWorldOutput(categoryHash, pos, resource->GetResource());
     V8_BIND_BASE_OBJECT(output, "Failed to create AudioOutputWorld");
 }
 
@@ -42,11 +45,12 @@ static void ConstructorAttached(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     V8_GET_ISOLATE_CONTEXT_RESOURCE();
     V8_CHECK_CONSTRUCTOR();
-    V8_CHECK_ARGS_LEN(1);
+    V8_CHECK_ARGS_LEN_MIN_MAX(1, 2);
 
     V8_ARG_TO_BASE_OBJECT(1, entity, alt::IWorldObject, "IWorldObject");
+    V8_ARG_TO_INT_OPT(2, categoryHash, alt::ICore::Instance().Hash("radio"));
 
-    auto output = alt::ICore::Instance().CreateAttachedOutput(0, entity, resource->GetResource());
+    auto output = alt::ICore::Instance().CreateAttachedOutput(categoryHash, entity, resource->GetResource());
     V8_BIND_BASE_OBJECT(output, "Failed to create AudioOutputAttached");
 }
 
@@ -64,6 +68,23 @@ static void AudioOutputCountGetter(v8::Local<v8::String> name, const v8::Propert
     V8_RETURN_UINT(alt::ICore::Instance().GetAudioOutputs().size());
 }
 
+static void EntityGetter(v8::Local<v8::String>, const v8::PropertyCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_GET_THIS_BASE_OBJECT(ent, alt::IAudioAttachedOutput);
+    V8_RETURN_BASE_OBJECT(ent->GetEntity());
+}
+
+static void EntitySetter(v8::Local<v8::String>, v8::Local<v8::Value> val, const v8::PropertyCallbackInfo<void>& info)
+{
+    V8_GET_ISOLATE_CONTEXT();
+    V8_GET_THIS_BASE_OBJECT(_this, alt::IAudioAttachedOutput);
+
+    V8_TO_WORLDOBJECT(val, entity);
+
+    _this->SetEntity(entity);
+}
+
 using namespace alt;
 
 extern V8Class v8BaseObject;
@@ -79,7 +100,7 @@ extern V8Class v8AudioOutput("AudioOutput",
 
         V8Helpers::SetAccessor<IAudioOutput, bool, &IAudioOutput::IsMuted, &IAudioOutput::SetMuted>(isolate, tpl, "muted");
         V8Helpers::SetAccessor<IAudioOutput, float, &IAudioOutput::GetVolume, &IAudioOutput::SetVolume>(isolate, tpl, "volume");
-        V8Helpers::SetAccessor<IAudioOutput, uint32_t, &IAudioOutput::GetCategory, &IAudioOutput::SetCategory>(isolate, tpl, "category");
+        V8Helpers::SetAccessor<IAudioOutput, uint32_t, &IAudioOutput::GetCategory>(isolate, tpl, "category");
     });
 
 
@@ -105,5 +126,6 @@ extern V8Class v8AudioOutputAttached("AudioOutputAttached",
     &ConstructorAttached,
     [](v8::Local<v8::FunctionTemplate> tpl)
     {
-
+        v8::Isolate* isolate = v8::Isolate::GetCurrent();
+        V8Helpers::SetAccessor(isolate, tpl, "entity", &EntityGetter, &EntitySetter);
     });
