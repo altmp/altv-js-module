d6f1ab917f19c48d4a2cc5e577b539b37bdf06a8
Mohammad Reza Gharabaghi <40915667+mrgharabaghi@users.noreply.github.com>
2023-07-15 22:37:35 +0100

    Add StaticGetByID to AudioOutput

--- client/src/bindings/AudioOutput.cpp
+++ client/src/bindings/AudioOutput.cpp
@@ -113,6 +113,25 @@ static void SetFilter(v8::Local<v8::String>, v8::Local<v8::Value> val, const v8:
     }
 }
 
+static void StaticGetByID(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_CHECK_ARGS_LEN(1);
+
+    V8_ARG_TO_INT(1, id);
+
+    alt::IBaseObject* entity = alt::ICore::Instance().GetBaseObjectByID(alt::IBaseObject::Type::AUDIO_OUTPUT, id);
+
+    if(entity)
+    {
+        V8_RETURN_BASE_OBJECT(entity);
+    }
+    else
+    {
+        V8_RETURN_NULL();
+    }
+}
+
 using namespace alt;
 
 extern V8Class v8BaseObject;
@@ -122,6 +141,8 @@ extern V8Class v8AudioOutput("AudioOutput",
     [](v8::Local<v8::FunctionTemplate> tpl)
     {
         v8::Isolate* isolate = v8::Isolate::GetCurrent();
+
+        V8Helpers::SetStaticMethod(isolate, tpl, "getByID", StaticGetByID);
 
         V8Helpers::SetStaticAccessor(isolate, tpl, "all", &AllAudioOutputGetter);
         V8Helpers::SetStaticAccessor(isolate, tpl, "count", &AudioOutputCountGetter);
