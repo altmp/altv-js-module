890d576daa4a2a93bdb2c092dce3d829ee15be5d
Blue <florian.uaz@web.de>
2023-07-21 21:41:38 +0200

    client: add localstorage has

--- client/src/bindings/LocalStorage.cpp
+++ client/src/bindings/LocalStorage.cpp
@@ -51,13 +51,27 @@ static void StaticSave(const v8::FunctionCallbackInfo<v8::Value>& info)
     V8_CHECK(resource->GetLocalStorage()->Save(), "exceeded max local storage size (4MB)");
 }
 
-extern V8Class v8LocalStorage("LocalStorage", nullptr, [](v8::Local<v8::FunctionTemplate> tpl) {
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
+static void StaticHas(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_IRESOURCE();
 
-    V8Helpers::SetStaticMethod(isolate, tpl, "get", &StaticGet);
-    V8Helpers::SetStaticMethod(isolate, tpl, "set", &StaticSet);
-    V8Helpers::SetStaticMethod(isolate, tpl, "delete", &StaticDelete);
-    V8Helpers::SetStaticMethod(isolate, tpl, "deleteAll", &StaticClear);
-    V8Helpers::SetStaticMethod(isolate, tpl, "clear", &StaticClear);
-    V8Helpers::SetStaticMethod(isolate, tpl, "save", &StaticSave);
-});
+    V8_CHECK_ARGS_LEN(1);
+    V8_ARG_TO_STRING(1, key);
+
+    V8_RETURN_BOOLEAN(resource->GetLocalStorage()->Has(key));
+}
+
+extern V8Class v8LocalStorage("LocalStorage",
+                              nullptr,
+                              [](v8::Local<v8::FunctionTemplate> tpl)
+                              {
+                                  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+
+                                  V8Helpers::SetStaticMethod(isolate, tpl, "get", &StaticGet);
+                                  V8Helpers::SetStaticMethod(isolate, tpl, "set", &StaticSet);
+                                  V8Helpers::SetStaticMethod(isolate, tpl, "delete", &StaticDelete);
+                                  V8Helpers::SetStaticMethod(isolate, tpl, "deleteAll", &StaticClear);
+                                  V8Helpers::SetStaticMethod(isolate, tpl, "clear", &StaticClear);
+                                  V8Helpers::SetStaticMethod(isolate, tpl, "save", &StaticSave);
+                                  V8Helpers::SetStaticMethod(isolate, tpl, "has", &StaticHas);
+                              });
