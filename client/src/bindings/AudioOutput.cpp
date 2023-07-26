fe0d9fea1d0389cc98fef76b6e09cc71f3b25ed3
Mohammad Reza Gharabaghi <40915667+mrgharabaghi@users.noreply.github.com>
2023-07-26 02:56:38 +0100

    client: Add AudioOutputWorld

--- client/src/bindings/AudioOutput.cpp
+++ client/src/bindings/AudioOutput.cpp
@@ -2,12 +2,12 @@
 #include "helpers/BindHelpers.h"
 #include "V8ResourceImpl.h"
 #include "V8Class.h"
+
 #include "../CV8ScriptRuntime.h"
+
 #include "cpp-sdk/objects/IWorldObject.h"
 #include "cpp-sdk/script-objects/IAudioOutput.h"
 #include "cpp-sdk/script-objects/IAudioFrontendOutput.h"
-#include "cpp-sdk/script-objects/IAudioWorldOutput.h"
-#include "cpp-sdk/script-objects/IAudioAttachedOutput.h"
 
 static void Constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
@@ -26,19 +26,6 @@ static void ConstructorFrontend(const v8::FunctionCallbackInfo<v8::Value>& info)
 
     auto output = alt::ICore::Instance().CreateFrontendOutput(categoryHash, resource->GetResource());
     V8_BIND_BASE_OBJECT(output, "Failed to create AudioOutputFrontend");
-}
-
-static void ConstructorWorld(const v8::FunctionCallbackInfo<v8::Value>& info)
-{
-    V8_GET_ISOLATE_CONTEXT_RESOURCE();
-    V8_CHECK_CONSTRUCTOR();
-    V8_CHECK_ARGS_LEN_MIN_MAX(1, 2);
-
-    V8_ARG_TO_VECTOR3(1, pos);
-    V8_ARG_TO_INT_OPT(2, categoryHash, alt::ICore::Instance().Hash("radio"));
-
-    auto output = alt::ICore::Instance().CreateWorldOutput(categoryHash, pos, resource->GetResource());
-    V8_BIND_BASE_OBJECT(output, "Failed to create AudioOutputWorld");
 }
 
 static void AllAudioOutputGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo<v8::Value>& info)
@@ -108,12 +95,3 @@ extern V8Class v8AudioOutputFrontend("AudioOutputFrontend",
                                      [](v8::Local<v8::FunctionTemplate> tpl) {
 
                                      });
-
-extern V8Class v8AudioOutputWorld("AudioOutputWorld",
-                                  v8AudioOutput,
-                                  &ConstructorWorld,
-                                  [](v8::Local<v8::FunctionTemplate> tpl)
-                                  {
-                                      v8::Isolate* isolate = v8::Isolate::GetCurrent();
-                                      V8Helpers::SetAccessor<IAudioWorldOutput, alt::Position, &IAudioWorldOutput::GetPosition, &IAudioWorldOutput::SetPosition>(isolate, tpl, "pos");
-                                  });
