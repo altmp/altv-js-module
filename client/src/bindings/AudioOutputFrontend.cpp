a83325fcedc648b28c8d91ce1d41a651645729f2
Mohammad Reza Gharabaghi <40915667+mrgharabaghi@users.noreply.github.com>
2023-07-30 23:30:26 +0100

    زمهclient: Add getByID to AudioOutputFrontend

--- client/src/bindings/AudioOutputFrontend.cpp
+++ client/src/bindings/AudioOutputFrontend.cpp
@@ -21,12 +21,34 @@ static void Constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
     V8_BIND_BASE_OBJECT(output, "Failed to create AudioOutputFrontend");
 }
 
+static void StaticGetByID(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_CHECK_ARGS_LEN(1);
+
+    V8_ARG_TO_INT(1, id);
+
+    alt::IBaseObject* baseObject = alt::ICore::Instance().GetBaseObjectByID(alt::IBaseObject::Type::AUDIO_OUTPUT_FRONTEND, id);
+
+    if(baseObject)
+    {
+        V8_RETURN_BASE_OBJECT(baseObject);
+    }
+    else
+    {
+        V8_RETURN_NULL();
+    }
+}
+
 using namespace alt;
 
 extern V8Class v8AudioOutput;
 extern V8Class v8AudioOutputFrontend("AudioOutputFrontend",
                                      v8AudioOutput,
                                      &Constructor,
-                                     [](v8::Local<v8::FunctionTemplate> tpl) {
+                                     [](v8::Local<v8::FunctionTemplate> tpl)
+                                     {
+                                         v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
+                                         V8Helpers::SetStaticMethod(isolate, tpl, "getByID", StaticGetByID);
                                      });
