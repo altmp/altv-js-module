c8a03ee8c69b7caf2c9394c62c15a25f35841728
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-09-04 00:01:16 +0200

    Add VirtualEntityGroup.getByID static methods

--- shared/bindings/VirtualEntityGroup.cpp
+++ shared/bindings/VirtualEntityGroup.cpp
@@ -17,6 +17,24 @@ static void Constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
     V8_BIND_BASE_OBJECT(virtualEntityGroup, "Failed to create virtual entity group");
 }
 
+static void StaticGetByID(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_CHECK_ARGS_LEN(1);
+
+    V8_ARG_TO_INT(1, id);
+
+    alt::IBaseObject* entity = alt::ICore::Instance().GetBaseObjectByID(alt::IBaseObject::Type::VIRTUAL_ENTITY_GROUP, id);
+
+    if(entity)
+    {
+        V8_RETURN_BASE_OBJECT(entity);
+        return;
+    }
+
+    V8_RETURN_NULL();
+}
+
 static void AllGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo<v8::Value>& info)
 {
     V8_GET_ISOLATE_CONTEXT_RESOURCE();
@@ -96,6 +114,8 @@ extern V8Class v8VirtualEntityGroup("VirtualEntityGroup",
                                     [](v8::Local<v8::FunctionTemplate> tpl)
                                     {
                                         v8::Isolate* isolate = v8::Isolate::GetCurrent();
+
+                                        V8Helpers::SetStaticMethod(isolate, tpl, "getByID", StaticGetByID);
 
                                         V8Helpers::SetStaticAccessor(isolate, tpl, "all", AllGetter);
 
