23fa683ad761aa06a15ed8b7fda418d38b6add20
Blue <florian.uaz@web.de>
2024-01-05 16:15:57 +0100

    fix(client): Textlabel allgetter

--- client/src/bindings/TextLabel.cpp
+++ client/src/bindings/TextLabel.cpp
@@ -26,11 +26,11 @@ static void Constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
     V8_BIND_BASE_OBJECT(textLabel, "Failed to create textlabel");
 }
 
-// static void AllGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo<v8::Value>& info)
-// {
-//     V8_GET_ISOLATE_CONTEXT_RESOURCE();
-//     V8_RETURN(resource->GetAllTextLabel());
-// }
+static void AllGetter(v8::Local<v8::String> name, const v8::PropertyCallbackInfo<v8::Value>& info)
+{
+    V8_GET_ISOLATE_CONTEXT_RESOURCE();
+    V8_RETURN(resource->GetAllTextLabel());
+}
 
 static void StaticGetByID(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
